54d9a550b45312deed302c1065c3042e00954738
vjacquet <vjacquet@flowgroup.fr>
2015-03-01 19:31:40 +0100

    Added decimal arithmetics.

---
+++ WmcSoft.Mathematics/Arithmetics/DecimalArithmetics.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmcSoft.Arithmetics
+{
+    public struct DecimalArithmetics : IArithmetics<decimal>
+    {
+        #region IArithmetics<decimal> Membres
+
+        public decimal Add(decimal x, decimal y) {
+            return x + y;
+        }
+
+        public decimal Subtract(decimal x, decimal y) {
+            return x - y;
+        }
+
+        public decimal Multiply(decimal x, decimal y) {
+            return x * y;
+        }
+
+        public decimal Divide(decimal x, decimal y) {
+            return x / y;
+        }
+
+        public decimal Remainder(decimal x, decimal y) {
+            return x % y;
+        }
+
+        public decimal DivideRemainder(decimal x, decimal y, out decimal remainder) {
+            remainder = x % y;
+            return x / y;
+        }
+
+        public decimal Negate(decimal x) {
+            return -x;
+        }
+
+        public decimal Reciprocal(decimal x) {
+            return 1m / x;
+        }
+
+        public decimal Zero {
+            get { return 0m; }
+        }
+
+        public decimal One {
+            get { return 1m; }
+        }
+
+        public bool SupportReciprocal {
+            get { return true; }
+        }
+
+        #endregion
+    }
+}
