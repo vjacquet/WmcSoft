45fc957917684c8e58106f8fbee6e8fe2031538b
vjacquet <vjacquet@flowgroup.fr>
2015-03-01 19:30:27 +0100

    Added quantiles

---
+++ WmcSoft.Core/WmcSoft.Mathematics.Tests/Statistics/QuantilesTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.Statistics.Tests
+{
+    [TestClass]
+    public class QuantilesTests
+    {
+        [TestMethod]
+        public void CanComputeQuartiles() {
+            var q = new Quantiles<int>(3, 6, 7, 8, 8, 10, 13, 15, 16, 20);
+
+            Assert.AreEqual(7, q[0.25]);
+            Assert.AreEqual(8, q[0.50]);
+            Assert.AreEqual(15, q[0.75]);
+        }
+    }
+}
