8767d58527ff033f8faff46b09b1cc81fe5bf034
vjacquet <vjacquet@flowgroup.fr>
2015-03-02 20:43:11 +0100

    Added more tests for BinaryFormatter.

--- WmcSoft.Core.Tests/BinaryFormatterTests.cs
+++ WmcSoft.Core.Tests/BinaryFormatterTests.cs
@@ -11,7 +11,7 @@ namespace WmcSoft.Tests
     public class BinaryFormatterTests
     {
         [TestMethod]
-        public void CanFormatByteWithH2() {
+        public void CanFormatByteWithX2() {
             var formatter = new BinaryFormatter();
 
             var expected = "A0";
@@ -22,7 +22,7 @@ namespace WmcSoft.Tests
         }
 
         [TestMethod]
-        public void CanFormatBytesWithH0() {
+        public void CanFormatBytesWithX0() {
             var formatter = new BinaryFormatter();
 
             var expected = "0A0B0C0D";
@@ -33,12 +33,45 @@ namespace WmcSoft.Tests
         }
 
         [TestMethod]
-        public void CanFormatBytesWithH2() {
+        public void CanFormatBytesWithX2() {
             var formatter = new BinaryFormatter();
 
             var expected = "0A0B 0C0D";
             var b = new byte[] { 0x0A, 0x0B, 0x0C, 0x0D, };
             var actual = String.Format(formatter, "{0:X2}", b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanFormatBytesWithB1() {
+            var formatter = new BinaryFormatter();
+
+            var expected = "00001010 00001011 00001100 00001101";
+            var b = new byte[] { 0x0A, 0x0B, 0x0C, 0x0D, };
+            var actual = String.Format(formatter, "{0:B1}", b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanFormatBytesWithB2() {
+            var formatter = new BinaryFormatter();
+
+            var expected = "0000101000001011 0000110000001101";
+            var b = new byte[] { 0x0A, 0x0B, 0x0C, 0x0D, };
+            var actual = String.Format(formatter, "{0:B2}", b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanFormatBytesWithO2() {
+            var formatter = new BinaryFormatter();
+
+            var expected = "00120013 00140015";
+            var b = new byte[] { 0x0A, 0x0B, 0x0C, 0x0D, };
+            var actual = String.Format(formatter, "{0:O2}", b);
 
             Assert.AreEqual(expected, actual);
         }
