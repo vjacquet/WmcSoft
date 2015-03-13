517e66be69c3b164b7388ce56483f394e43a0156
vjacquet <vjacquet@flowgroup.fr>
2015-03-13 21:35:39 +0100

    Added missing file.

---
+++ WmcSoft.Units/KnownScaledUnit.cs
@@ -0,0 +1,64 @@
+#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+
+namespace WmcSoft.Units
+{
+    /// <summary>
+    /// Description résumée de KnownScaledUnit.
+    /// </summary>
+    internal class KnownScaledUnit : ScaledUnit
+    {
+        public KnownScaledUnit(KnownScaledUnitPrefix prefix, Unit reference)
+            : base(RM.FormatSIPrefixName((int)prefix, reference.Name), RM.FormatSIPrefixSymbol((int)prefix, reference.Symbol), null, new Decimal(Math.Pow(10.0, (double)(int)prefix)), reference) {
+        }
+    }
+
+    public enum KnownScaledUnitPrefix
+    {
+        Yotta = 24,
+        Zetta = 21,
+        Exa = 18,
+        Peta = 15,
+        Tera = 12,
+        Giga = 9,
+        Mega = 6,
+        Kilo = 3,
+        Hecto = 2,
+        Deka = 1,
+        Deci = -1,
+        Centi = -2,
+        Milli = -3,
+        Micro = -6,
+        Nano = -9,
+        Pico = -12,
+        Femto = -15,
+        Atto = -18,
+        Zepto = -21,
+        Yocto = -24,
+    }
+}
