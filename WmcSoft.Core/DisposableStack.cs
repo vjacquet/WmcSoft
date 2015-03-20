2b70243ca264346b41d230425e9a36d3edee765c
vjacquet <vjacquet@flowgroup.fr>
2015-03-20 12:24:28 +0100

    Added DisposableStack.

---
+++ WmcSoft.Core/DisposableStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmcSoft
+{
+    /// <summary>
+    /// Stack disposables to Dispose them in reverse order.
+    /// </summary>
+    public class DisposableStack : IDisposable
+    {
+        readonly Stack<IDisposable> _stack;
+
+        public DisposableStack() {
+            _stack = new Stack<IDisposable>();
+        }
+
+        ~DisposableStack() {
+            Dispose(false);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Push(IDisposable disposable) {
+            _stack.Push(disposable);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            while (_stack.Count != 0) {
+                _stack.Pop().Dispose();
+            }
+        }
+    }
+}
