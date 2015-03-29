1747c3356510f0651304369d9abb8c530d45043c
vjacquet <vjacquet@flowgroup.fr>
2015-03-29 11:50:52 +0200

    Added tests for the CommandLine.

--- WmcSoft.Components.Tests/CommandLine/CommandLineTests.cs
+++ WmcSoft.Components.Tests/CommandLine/CommandLineTests.cs
@@ -11,9 +11,12 @@ namespace WmcSoft.CommandLine.Tests
             var bench =new BenchComponent();
             var commandLine = bench.commandLine;
 
-            var parsed = commandLine.ParseArguments("/string:test");
-            var actual = commandLine.Options["string"].Value.ToString();
-            Assert.AreEqual("test", actual);
+            var parsed = commandLine.ParseArguments("/string:test", "/choice:A", "/switch", "/boolean-");
+            Assert.AreEqual("test", commandLine.Options["string"].Value.ToString());
+            Assert.AreEqual("A", commandLine.Options["choice"].Value.ToString());
+            Assert.AreEqual(true, commandLine.Options["switch"].IsPresent);
+            Assert.AreEqual(true, commandLine.Options["boolean"].IsPresent);
+            Assert.AreEqual(true, commandLine.Options["boolean"].Value.Equals(false));
         }
     }
 }
