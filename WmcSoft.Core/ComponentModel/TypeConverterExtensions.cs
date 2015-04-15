4b9f130ba0d7c1ae8c6295aecd65d1f60e922d67
vjacquet <vjacquet@flowgroup.fr>
2015-04-15 20:57:45 +0200

    Added Delegate and TypeConverter extensions.

---
+++ WmcSoft.Core/ComponentModel/TypeConverterExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmcSoft.ComponentModel
+{
+   public static class TypeConverterExtensions
+    {
+       public static bool CanConvertTo<T>(this TypeConverter converter) {
+           return converter.CanConvertTo(typeof(T));
+       }
+       public static bool CanConvertTo<T>(this TypeConverter converter, ITypeDescriptorContext context) {
+           return converter.CanConvertTo(context, typeof(T));
+       }
+
+       public static T ConvertTo<T>(this TypeConverter converter, object value) {
+           return (T)converter.ConvertTo(value, typeof(T));
+       }
+       public static T ConvertTo<T>(this TypeConverter converter, ITypeDescriptorContext context, CultureInfo culture, object value) {
+           return (T)converter.ConvertTo(context, culture, value, typeof(T));
+       }
+    }
+}
