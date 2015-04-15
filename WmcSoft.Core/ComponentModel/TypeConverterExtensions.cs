4d20e4da09d4665d8f37f5f7ff28f34b4a25fdf6
vjacquet <vjacquet@flowgroup.fr>
2015-04-15 21:08:28 +0200

    More TypeConverter extensions.

--- WmcSoft.Core/ComponentModel/TypeConverterExtensions.cs
+++ WmcSoft.Core/ComponentModel/TypeConverterExtensions.cs
@@ -8,20 +8,27 @@ using System.Threading.Tasks;
 
 namespace WmcSoft.ComponentModel
 {
-   public static class TypeConverterExtensions
+    public static class TypeConverterExtensions
     {
-       public static bool CanConvertTo<T>(this TypeConverter converter) {
-           return converter.CanConvertTo(typeof(T));
-       }
-       public static bool CanConvertTo<T>(this TypeConverter converter, ITypeDescriptorContext context) {
-           return converter.CanConvertTo(context, typeof(T));
-       }
+        public static bool CanConvertFrom<T>(this TypeConverter converter) {
+            return converter.CanConvertFrom(typeof(T));
+        }
+        public static bool CanConvertFrom<T>(this TypeConverter converter, ITypeDescriptorContext context) {
+            return converter.CanConvertFrom(context, typeof(T));
+        }
 
-       public static T ConvertTo<T>(this TypeConverter converter, object value) {
-           return (T)converter.ConvertTo(value, typeof(T));
-       }
-       public static T ConvertTo<T>(this TypeConverter converter, ITypeDescriptorContext context, CultureInfo culture, object value) {
-           return (T)converter.ConvertTo(context, culture, value, typeof(T));
-       }
+        public static bool CanConvertTo<T>(this TypeConverter converter) {
+            return converter.CanConvertTo(typeof(T));
+        }
+        public static bool CanConvertTo<T>(this TypeConverter converter, ITypeDescriptorContext context) {
+            return converter.CanConvertTo(context, typeof(T));
+        }
+
+        public static T ConvertTo<T>(this TypeConverter converter, object value) {
+            return (T)converter.ConvertTo(value, typeof(T));
+        }
+        public static T ConvertTo<T>(this TypeConverter converter, ITypeDescriptorContext context, CultureInfo culture, object value) {
+            return (T)converter.ConvertTo(context, culture, value, typeof(T));
+        }
     }
 }
