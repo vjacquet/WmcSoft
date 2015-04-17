e6f6eb0d1eb36b0c373edc0c7a57b228c02a8f3c
vjacquet <vjacquet@flowgroup.fr>
2015-04-17 20:51:16 +0200

    Added EnumerateFileStreamsInfo.

---
+++ WmcSoft.Core/IO/FileInfoExtensions.cs
@@ -0,0 +1,127 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace WmcSoft.IO
+{
+    public static class FileInfoExtensions
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct Win32StreamID
+        {
+            public int dwStreamId;
+            public int dwStreamAttributes;
+            public long Size;
+            public int dwStreamNameSize;
+        }
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool BackupRead(SafeFileHandle hFile, IntPtr lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, [MarshalAs(UnmanagedType.Bool)] bool bAbort, [MarshalAs(UnmanagedType.Bool)] bool bProcessSecurity, ref IntPtr lpContext);
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool BackupSeek(SafeFileHandle hFile, uint dwLowBytesToSeek, uint dwHighBytesToSeek, out uint lpdwLowByteSeeked, out uint lpdwHighByteSeeked, ref IntPtr lpContext);
+
+        public static IEnumerable<FileStreamInfo> EnumerateFileStreamsInfo(this FileInfo file) {
+            const int bufferSize = 4096;
+            using (FileStream fs = file.OpenRead()) {
+                IntPtr context = IntPtr.Zero;
+                IntPtr buffer = Marshal.AllocHGlobal(bufferSize);
+                try {
+                    while (true) {
+                        uint numRead;
+                        if (!BackupRead(fs.SafeFileHandle, buffer,
+                           (uint)Marshal.SizeOf(typeof(Win32StreamID)), out numRead,
+                           false, true, ref context))
+                            throw new Win32Exception();
+
+                        if (numRead > 0) {
+                            Win32StreamID streamID = (Win32StreamID)
+                              Marshal.PtrToStructure(buffer, typeof(Win32StreamID));
+                            string name = null;
+                            if (streamID.dwStreamNameSize > 0) {
+                                if (!BackupRead(fs.SafeFileHandle, buffer,
+                                     (uint)System.Math.Min(bufferSize, streamID.dwStreamNameSize),
+                                     out numRead, false, true, ref context))
+                                    throw new Win32Exception();
+                                name = Marshal.PtrToStringUni(buffer, (int)numRead / 2);
+                            }
+
+                            yield return new FileStreamInfo(name, (FileStreamType)streamID.dwStreamId, streamID.Size);
+
+                            if (streamID.Size > 0) {
+                                uint lo, hi;
+                                BackupSeek(fs.SafeFileHandle, uint.MaxValue,
+                                  int.MaxValue, out lo, out hi, ref context);
+                            }
+                        } else
+                            break;
+                    }
+                }
+                finally {
+                    Marshal.FreeHGlobal(buffer);
+                    uint numRead;
+                    if (!BackupRead(fs.SafeFileHandle, IntPtr.Zero, 0, out numRead,
+                         true, false, ref context))
+                        throw new Win32Exception();
+                }
+            }
+        }
+    }
+
+    public enum FileStreamType
+    {
+        Data = 1,
+        ExternalData = 2,
+        SecurityData = 3,
+        AlternateData = 4,
+        Link = 5,
+        PropertyData = 6,
+        ObjectID = 7,
+        ReparseData = 8,
+        SparseDock = 9
+    }
+
+    public class FileStreamInfo
+    {
+        public FileStreamInfo(string name, FileStreamType streamType, long size) {
+            Name = name;
+            StreamType = streamType;
+            Size = size;
+        }
+        public string Name { get; private set; }
+        public FileStreamType StreamType { get; private set; }
+        public long Size { get; private set; }
+    }
+
+}
--- WmcSoft.Core/IO/FileSystemInfoExtensions.cs
+++ WmcSoft.Core/IO/FileSystemInfoExtensions.cs
@@ -24,12 +24,7 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Win32;
 
 namespace WmcSoft.IO
