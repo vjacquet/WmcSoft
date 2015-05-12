e8c0e3216a41de85be6d420bea785f9b1c38376a
vjacquet <vjacquet@flowgroup.fr>
2015-05-12 06:19:08 +0200

    Added ReflectionExtensions.

---
+++ WmcSoft.Core.Tests/Reflection/TypeExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.Reflection.Tests
+{
+    [TestClass]
+    public class TypeExtensionsTests
+    {
+        [TestMethod]
+        public void AssertIsOverriden() {
+            var m = typeof(Uri).GetMethod("GetHashCode", BindingFlags.Public | BindingFlags.Instance);
+            var actual = m.DeclaringType != typeof(Object);
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void CheckEnumerateDefinitions() {
+            var m = typeof(Uri).GetMethod("GetHashCode", BindingFlags.Public | BindingFlags.Instance);
+            var expected = new Type[] { typeof(Uri), typeof(object) };
+            var actual = m.EnumerateDefinitions().Select(d => d.DeclaringType).ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ WmcSoft.Core/Reflection/ReflectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WmcSoft.Collections.Generic;
+
+namespace WmcSoft.Reflection
+{
+    public static class ReflectionExtensions
+    {
+        public static IEnumerable<MethodInfo> EnumerateDefinitions(this MethodInfo method) {
+            var baseDefinition = method.GetBaseDefinition();
+            var parameters = method.GetParameters().ToArray(p => p.ParameterType);
+            while (method != baseDefinition) {
+                yield return method;
+                method = method.DeclaringType.BaseType.GetMethod(method.Name, parameters);
+            }
+            yield return baseDefinition;
+        }
+    }
+}
