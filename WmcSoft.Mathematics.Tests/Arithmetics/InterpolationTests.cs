2a8c4680fec9b3face1bae1835d04eb127af69b8
vjacquet <vjacquet@flowgroup.fr>
2015-06-09 20:46:36 +0200

    Fixed LinearInterpolation.

---
+++ WmcSoft.Mathematics.Tests/Arithmetics/InterpolationTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WmcSoft.Algebra;
+
+namespace WmcSoft.Arithmetics
+{
+    [TestClass]
+    public class InterpolationTests
+    {
+        [TestMethod]
+        public void CheckLinearInterpolation() {
+            double[] x = { 1d, 2d, 3d };
+            double[] y = { 2d, 4d, 6d };
+            var i = new LinearInterpolation(x, y);
+            var actual = i.Interpolate(1.5d);
+            Assert.AreEqual(3d, actual);
+        }
+
+        [TestMethod]
+        public void CheckLinearInterpolationWhenExact() {
+            double[] x = { 1d, 2d, 3d };
+            double[] y = { 2d, 4d, 6d };
+            var i = new LinearInterpolation(x, y);
+            var actual = i.Interpolate(2d);
+            Assert.AreEqual(4d, actual);
+        }
+        
+        [TestMethod]
+        public void CheckLinearInterpolationWhenSmaller() {
+            double[] x = { 1d, 2d, 3d };
+            double[] y = { 2d, 4d, 6d };
+            var i = new LinearInterpolation(x, y);
+            var actual = i.Interpolate(0d);
+            Assert.AreEqual(0d, actual);
+        }
+
+        [TestMethod]
+        public void CheckLinearInterpolationWhenBigger() {
+            double[] x = { 1d, 2d, 3d };
+            double[] y = { 2d, 4d, 6d };
+            var i = new LinearInterpolation(x, y);
+            var actual = i.Interpolate(4d);
+            Assert.AreEqual(8d, actual);
+        }
+
+    }
+}
--- WmcSoft.Mathematics/Arithmetics/LinearInterpolation.cs
+++ WmcSoft.Mathematics/Arithmetics/LinearInterpolation.cs
@@ -1,4 +1,29 @@
-﻿
+#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +35,7 @@ namespace WmcSoft.Arithmetics
     ///<summary>
     /// Linear interpolation is a method of curve fitting using linear polynomials.
     ///</summary>
-    public class LinearInterpolation : IInterpolation<double,double>
+    public class LinearInterpolation : IInterpolation<double, double>
     {
         #region fields
 
@@ -56,13 +81,14 @@ namespace WmcSoft.Arithmetics
             if (offset >= 0)
                 return _values[offset];
 
-            int start = ~offset;
-            if (start >= _points.Length) {
-                start = _points.Length - 1;
-            }
-            int end = start + 1;
+            int end = ~offset;
+            if (end >= _points.Length)
+                end = _points.Length - 1;
+            else if (end == 0)
+                end = 1;
+            int start = end - 1;
 
-            return _values[start] + (_values[end] - _values[start]) / (_points[end] - _points[start]);
+            return _values[start] + (x - _points[start]) * (_values[end] - _values[start]) / (_points[end] - _points[start]);
         }
 
         #endregion
