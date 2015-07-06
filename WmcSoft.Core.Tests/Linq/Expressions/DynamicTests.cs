74fc59240815c23bf8f1e5d22bcc3de7e45db275
vjacquet <vjacquet@flowgroup.fr>
2015-07-06 19:38:53 +0200

    Added Dynamic tests.

---
+++ WmcSoft.Core.Tests/Linq/Expressions/DynamicTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Dynamic;
+
+namespace WmcSoft.Linq.Expressions
+{
+    [TestClass]
+    public class DynamicTests
+    {
+        [TestMethod]
+        public void CanParseWhere() {
+            var collection = new[] { "a", "b", "c", "D", "e", "F", "g" };
+            var queryable = collection.AsQueryable();
+            var result = String.Concat(queryable.Where("it.ToUpper() == it").ToArray());
+            Assert.AreEqual("DF", result);
+        }
+    }
+}
