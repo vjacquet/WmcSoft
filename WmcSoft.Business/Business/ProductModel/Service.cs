fb1b2d042096c11f51646c7e6d0a0e745f60879e
vjacquet <vjacquet@flowgroup.fr>
2015-07-07 19:09:14 +0200

    Added RegistrationPolicy.

--- WmcSoft.Business/Business/ProductModel/Service.cs
+++ WmcSoft.Business/Business/ProductModel/Service.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WmcSoft
+namespace FlowGroup.Business.ProductModel
 {
-    interface IUniqueIdentifier<T> where T : IEquatable<T>
+    public abstract class Service : ProductType
     {
-        T Id { get; }
     }
 }
