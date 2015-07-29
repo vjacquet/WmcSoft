5bc42a7a2c437ed1195bc4d2cdf4864fa037d24f
vjacquet <vjacquet@flowgroup.fr>
2015-07-29 13:24:49 +0200

    Added IOrdinal.

---
+++ WmcSoft.Core/IOrdinal.cs
@@ -0,0 +1,82 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmcSoft
+{
+    /// <summary>
+    /// Defines an order on elements with means to jump from one to another.
+    /// </summary>
+    /// <typeparam name="T">The type of items to order.</typeparam>
+    /// <remrks>The Compare method returns the distance between two elements.</remrks>
+    public interface IOrdinal<in T> : IComparer<T>
+    {
+        T Advance(T x, int n);
+    }
+
+    public struct Int32Ordinal : IOrdinal<int>
+    {
+        #region IOrdinal<int> Members
+
+        public int Advance(int x, int n) {
+            return checked(x + n);
+        }
+
+        #endregion
+
+        #region IComparer<int> Members
+
+        public int Compare(int x, int y) {
+            return checked(y - x);
+        }
+
+        #endregion
+    }
+
+    public struct DateTimeOrdinal : IOrdinal<DateTime>
+    {
+        #region IOrdinal<DateTime> Members
+
+        public DateTime Advance(DateTime x, int n) {
+            return x.AddDays(n);
+        }
+
+        #endregion
+
+        #region IComparer<DateTime> Members
+
+        public int Compare(DateTime x, DateTime y) {
+            return (int)Math.Truncate((y - x).TotalDays);
+        }
+
+        #endregion
+    }
+}
