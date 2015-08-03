f5ea5fe927f8491ea079359b64a6eca492c6afcb
vjacquet <vjacquet@flowgroup.fr>
2015-08-03 21:38:50 +0200

    Added DisposableSet and more DataSet extensions.

---
+++ WmcSoft.Core/DisposableSet.cs
@@ -0,0 +1,66 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace WmcSoft
+{
+    /// <summary>
+    /// Stack disposables to Dispose them in reverse order. 
+    /// </summary>
+    /// <remarks>An IDisposable is added only once.</remarks>
+    public class DisposableSet : IDisposable
+    {
+        readonly HashSet<IDisposable> _set;
+        readonly Stack<IDisposable> _stack;
+
+        public DisposableSet() {
+            _set = new HashSet<IDisposable>();
+            _stack = new Stack<IDisposable>();
+        }
+
+        ~DisposableSet() {
+            Dispose(false);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Push(IDisposable disposable) {
+            if (_set.Add(disposable))
+                _stack.Push(disposable);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            while (_stack.Count != 0) {
+                _stack.Pop().Dispose();
+            }
+        }
+    }
+}
