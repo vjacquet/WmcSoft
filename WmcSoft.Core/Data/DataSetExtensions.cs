fdbf1a81cf69948d36f6fc48957b97f4526fc9fa
vjacquet <vjacquet@flowgroup.fr>
2015-08-03 20:58:59 +0200

    Added DataSet extensions.

---
+++ WmcSoft.Core/Data/DataSetExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmcSoft.Data
+{
+    public static class DataSetExtensions
+    {
+        public static IEnumerable<DataRow> Where(this DataRowCollection collection, Func<DataRow, bool> predicate) {
+            return collection.Cast<DataRow>().Where(predicate);
+        }
+
+        public static IEnumerable<DataRow> Where(this DataRowCollection collection, Func<DataRow, int, bool> predicate) {
+            return collection.Cast<DataRow>().Where(predicate);
+        }
+    }
+}
