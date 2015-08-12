43ecc9c48b0f1979b80c6b68d16b40d1bd446b47
vjacquet <vjacquet@flowgroup.fr>
2015-08-12 07:12:38 +0200

    Added ToDictionary with duplicate policy extensions.

---
+++ WmcSoft.Core/SpecialFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmcSoft
+{
+    internal class SpecialFunctions<TElement>
+    {
+        public static Func<TElement, TElement> Identity {
+            get { return x => x; }
+        }
+
+        public static Predicate<TElement> Always {
+            get { return _ => true; }
+        }
+
+        public static Predicate<TElement> Never {
+            get { return _ => false; }
+        }
+    }
+}
