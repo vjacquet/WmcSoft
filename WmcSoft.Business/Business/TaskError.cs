5a87d441654128ffb65350ce88f29a12a0044a48
vjacquet <vjacquet@flowgroup.fr>
2015-08-28 23:46:11 +0200

    Added TraceTrackingHandler.

---
+++ WmcSoft.Business/Business/TaskError.cs
@@ -0,0 +1,46 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+
+namespace WmcSoft.Business
+{
+    public class TaskError
+    {
+        readonly string _code;
+        readonly string _description;
+
+        public TaskError(string code, string description) {
+            if (code == null)
+                throw new ArgumentNullException("code");
+
+            _code = code;
+            _description = description ?? code;
+        }
+        public string Code { get { return _code; } }
+        public string Description { get; set; }
+    }
+}
