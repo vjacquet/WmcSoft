9f97e6df4d0cc1a936e035d95d44f07fdb4c5b87
vjacquet <vjacquet@flowgroup.fr>
2015-09-12 19:28:59 +0200

    Added HSL tests.

---
+++ WmcSoft.Components.Tests/Drawing/HSLColorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.Drawing
+{
+    [TestClass]
+    public class HSLColorTests
+    {
+        [TestMethod]
+        public void CanConvertHSL2RGB() {
+            var expected = Color.FromArgb(32, 64, 128);
+            var hsl = new HSLColor(expected);
+            var actual = (Color)hsl;
+            //Assert.AreEqual(expected, actual);
+            Assert.IsTrue(Math.Abs(expected.R - actual.R) <= 1);
+            Assert.IsTrue(Math.Abs(expected.G - actual.G) <= 1);
+            Assert.IsTrue(Math.Abs(expected.B - actual.B) <= 1);
+        }
+    }
+}
