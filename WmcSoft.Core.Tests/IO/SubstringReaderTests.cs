14b2bbe5e442e3235a80ffaad65f2e075ab6449c
vjacquet <vjacquet@flowgroup.fr>
2015-09-28 22:09:19 +0200

    Added SubstringReader and basic Strip class.

---
+++ WmcSoft.Core.Tests/IO/SubstringReaderTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.IO
+{
+    [TestClass]
+    public class SubstringReaderTests
+    {
+        [TestMethod]
+        public void CanPeek() {
+            var data = "abcdefghijklmnopqrstuvwxyz";
+            using (var reader = new SubstringReader(data, 1, 5)) {
+                Assert.AreEqual('b', reader.Peek());
+                Assert.AreEqual('b', reader.Read());
+                Assert.AreEqual('c', reader.Peek());
+            }
+        }
+
+        [TestMethod]
+        public void CanReadToEnd() {
+            var data = "abcdefghijklmnopqrstuvwxyz";
+            using (var reader = new SubstringReader(data, 1, 5)) {
+                const string expected = "bcdef";
+                var actual = reader.ReadToEnd();
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod]
+        public void CanReadLines() {
+            var data = "abcde\r\nfghij\r\nklmno\r\npqrs\r\ntuvxy";
+            using (var reader = new SubstringReader(data, 0, 16)) {
+                Assert.AreEqual("abcde", reader.ReadLine());
+                Assert.AreEqual("fghij", reader.ReadLine());
+                Assert.AreEqual("kl", reader.ReadLine());
+                Assert.AreEqual(null, reader.ReadLine());
+            }
+        }
+
+        [TestMethod]
+        public void CanReadLinesWhenLastLineIsAnEmptyLine() {
+            var data = "abcde\r\nfghij\r\nklmno\r\npqrs\r\ntuvxy";
+            using (var reader = new SubstringReader(data, 0, 14)) {
+                Assert.AreEqual("abcde", reader.ReadLine());
+                Assert.AreEqual("fghij", reader.ReadLine());
+                Assert.AreEqual(null, reader.ReadLine());
+            }
+        }
+    }
+}
---
+++ WmcSoft.Core/Text/Strip.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmcSoft.Text
+{
+    public sealed class Strip : IComparable<string>, IReadOnlyList<char>
+    {
+        #region Fields
+
+        private readonly string _s;
+        private readonly int _start;
+        private readonly int _end;
+
+        #endregion
+
+        #region Lifecycle
+
+        public Strip(string s, int startIndex, int length) {
+            _s = s;
+            _start = startIndex;
+            _end = startIndex + length;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Length {
+            get { return _end - _start; }
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override string ToString() {
+            return _s.Substring(_start, Length);
+        }
+
+        public int CompareTo(string other) {
+            return String.Compare(_s, _start, other, 0, Length);
+        }
+
+        #endregion
+
+        #region IReadOnlyList<char> Members
+
+        int IReadOnlyCollection<char>.Count {
+            get { return Length; }
+        }
+
+        public char this[int index] {
+            get { return _s[_start + index]; }
+        }
+
+        public IEnumerator<char> GetEnumerator() {
+            for (int i = _start; i < _end; i++) {
+                yield return _s[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
