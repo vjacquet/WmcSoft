af89dbbbce7fc8b6abb244301a85260e53a4db74
vjacquet <vjacquet@flowgroup.fr>
2015-10-04 10:25:20 +0200

    Added Rotate algorithm.

--- WmcSoft.Core.Tests/IndexedAlgorithmsTests.cs
+++ WmcSoft.Core.Tests/IndexedAlgorithmsTests.cs
@@ -16,9 +16,18 @@ namespace WmcSoft
         [TestMethod]
         public void CheckCopyBackwardsTo() {
             var expected = new[] { 1, 4, 3, 2, 5 };
-            var data =  new[] { 1, 2, 3, 4, 5 };
+            var data = new[] { 1, 2, 3, 4, 5 };
             var actual = new[] { 1, 0, 0, 0, 5 };
             data.CopyBackwardsTo(1, actual, 1, 3);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckRotate() {
+            var expected = new[] { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
+            var actual = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var startIndex = actual.Rotate(3);
+            Assert.AreEqual(6, startIndex);
             CollectionAssert.AreEqual(expected, actual);
         }
     }
--- WmcSoft.Core/IndexedAlgorithms.cs
+++ WmcSoft.Core/IndexedAlgorithms.cs
@@ -8,6 +8,17 @@ namespace WmcSoft
 {
     public static class IndexedAlgorithms
     {
+        #region Helpers
+
+        static internal void UncheckedCopy<T>(IList<T> source, int sourceIndex, IList<T> destination, int destinationIndex, int length) {
+            var end = sourceIndex + length;
+            for (int i = sourceIndex, j = destinationIndex; i < end; i++, j++) {
+                destination[j] = source[i];
+            }
+        }
+
+        #endregion
+
         #region CopyTo methods
 
         public static void CopyTo<T>(this IList<T> source, T[] array, int arrayIndex, int length) {
@@ -81,6 +92,47 @@ namespace WmcSoft
             }
         }
 
+        #endregion
+
+        #region Rotate
+
+        /// <summary>
+        /// Performs a left rotation on the <paramref name="source"/>, moving the <paramref name="n"/> first elements of the specified range 
+        /// at the end.
+        /// </summary>
+        /// <typeparam name="T">The type of the elemets</typeparam>
+        /// <param name="source">The source sequence</param>
+        /// <param name="n">The number of elements to move</param>
+        /// <param name="startIndex">The start index of the range</param>
+        /// <param name="length">The length of the range</param>
+        /// <returns>The new position of the first element</returns>
+        public static int Rotate<T>(this IList<T> source, int n, int startIndex, int length) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (length < 0) throw new ArgumentOutOfRangeException("length");
+            if (n < 0) throw new ArgumentOutOfRangeException("n");
+            if (n > length) throw new ArgumentException("n");
+            if (source.Count < (startIndex + length)) throw new ArgumentException("source");
+
+            var temp = new T[n];
+            UncheckedCopy(source, startIndex, temp, 0, n);
+            UncheckedCopy(source, startIndex + n, source, startIndex, length - n);
+            startIndex += length - n;
+            UncheckedCopy(temp, 0, source, startIndex, n);
+            return startIndex;
+        }
+
+        /// <summary>
+        /// Performs a left rotation on the <paramref name="source"/>, moving the <paramref name="n"/> first elements at the end.
+        /// </summary>
+        /// <typeparam name="T">The type of the elemets</typeparam>
+        /// <param name="source">The source sequence</param>
+        /// <param name="n">The number of elements to move</param>
+        /// <returns>The new position of the first element</returns>
+        public static int Rotate<T>(this IList<T> source, int n) {
+            if (source == null) throw new ArgumentNullException("source");
+
+            return Rotate(source, n, 0, source.Count);
+        }
         #endregion
 
         #region SwapItems methods
