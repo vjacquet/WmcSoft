2c374fae4aea863ace17d634e8b65a247bcf0f38
vjacquet <vjacquet@flowgroup.fr>
2015-11-20 19:53:29 +0100

    Added BitArithmetics.

---
+++ WmcSoft.Core.Tests/BitArithmeticsTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static WmcSoft.BitArithmetics;
+
+namespace WmcSoft
+{
+    [TestClass]
+    public class BitArithmeticsTests
+    {
+        [TestMethod]
+        public void CanTurnOffRightMostOne() {
+            uint x = 0x58; // 0101 1000;
+            uint y = 0x50; // 0101 0000;
+            Assert.AreEqual(y, TurnOffRightMostOne(x));
+        }
+
+        [TestMethod]
+        public void CanTurnOnRightMostZero() {
+            uint x = 0xC7; // 1010 0111;
+            uint y = 0xCF; // 1010 1111;
+            Assert.AreEqual(y, TurnOnRightMostZero(x));
+        }
+
+        [TestMethod]
+        public void CanTurnOffTrailingOnes() {
+            uint x = 0xC7; // 1010 0111;
+            uint y = 0xC0; // 1010 0000;
+            Assert.AreEqual(y, TurnOffTrailingOnes(x));
+        }
+
+        [TestMethod]
+        public void CanTurnOnTrailingZeroes() {
+            uint x = 0xC8; // 1010 1000;
+            uint y = 0xCF; // 1010 1111;
+            Assert.AreEqual(y, TurnOnTrailingZeroes(x));
+        }
+
+        [TestMethod]
+        public void CanMarkRightMostZero() {
+            uint x = 0xC7; // 1010 0111;
+            uint y = 0x08; // 0000 1000;
+            Assert.AreEqual(y, MarkRightMostZero(x));
+        }
+
+        [TestMethod]
+        public void CanMarkRightMostOne() {
+            uint x = 0xC8; // 1010 1000;
+            uint y = 0x08; // 0000 1000;
+            Assert.AreEqual(y, MarkRightMostOne(x));
+        }
+    }
+}
---
+++ WmcSoft.Core/BitArithmetics.cs
@@ -0,0 +1,63 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+namespace WmcSoft
+{
+    /// <summary>
+    /// Formula taken from Hacker's Delight, second edition, by Henry S. Warren, Jr.
+    /// </summary>
+    public static class BitArithmetics
+    {
+        public static uint TurnOffRightMostOne(uint x) {
+            return x & (x - 1);
+        }
+
+        public static uint TurnOnRightMostZero(uint x) {
+            return x | (x + 1);
+        }
+
+        public static uint TurnOffTrailingOnes(uint x) {
+            return x & (x + 1);
+        }
+
+        public static uint TurnOnTrailingZeroes(uint x) {
+            return x | (x - 1);
+        }
+
+        public static uint MarkRightMostZero(uint x) {
+            return ~x & (x + 1);
+        }
+
+        public static uint MarkRightMostOne(uint x) {
+            return ~(~x | (x - 1));
+        }
+
+        public static bool IsPowerOfTwo(uint x) {
+            return 0 == (x & (x - 1));
+        }
+
+    }
+}
