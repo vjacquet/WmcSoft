e97bf68f8995474b52a83bc9f77f1a79db5b569a
vjacquet <vjacquet@flowgroup.fr>
2015-12-14 19:12:47 +0100

    Test CounterClockwise method.

--- WmcSoft.Mathematics.Tests/Geometry2D/PointTests.cs
+++ WmcSoft.Mathematics.Tests/Geometry2D/PointTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static WmcSoft.Geometry2D.Helpers;
 
 namespace WmcSoft.Geometry2D
 {
@@ -25,6 +26,16 @@ namespace WmcSoft.Geometry2D
             Assert.AreEqual("P1", p1.ToString("N", iv));
             Assert.AreEqual("(2,4)", p1.ToString("C", iv));
             Assert.AreEqual("P1 (2,4)", p1.ToString(iv));
+        }
+
+        [TestMethod]
+        public void CanTurnCounterClockwise() {
+            var p0 = new Point(2d, 0);
+            var p1 = new Point(1.5d, 1d);
+            var p2 = new Point(0.5d, 1d);
+
+            Assert.AreEqual(1, CounterClockwise(p0, p1, p2));
+            Assert.AreEqual(-1, CounterClockwise(p2, p1, p0));
         }
     }
 }
