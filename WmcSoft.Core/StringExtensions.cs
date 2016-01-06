322eec30c3a6cacd8f8d90a874b678053dcfb363
vjacquet <vjacquet@flowgroup.fr>
2016-01-06 20:09:34 +0100

    Added RemoveAffixes with both prefix and suffix.

--- WmcSoft.Core/StringExtensions.cs
+++ WmcSoft.Core/StringExtensions.cs
@@ -614,6 +614,72 @@ namespace WmcSoft
 
         #endregion
 
+        #region RemovePrefix/RemoveSuffix/RemoveAffixes
+
+        public static string RemovePrefix(this string self, string prefix) {
+            return RemovePrefix(self, prefix, StringComparison.CurrentCulture);
+        }
+
+        public static string RemovePrefix(this string self, string prefix, StringComparison comparison) {
+            if (self != null && self.StartsWith(prefix, comparison))
+                return self.Substring(prefix.Length);
+            return self;
+        }
+
+        public static string RemoveSuffix(this string self, string suffix) {
+            return RemoveSuffix(self, suffix, StringComparison.CurrentCulture);
+        }
+
+        public static string RemoveSuffix(this string self, string suffix, StringComparison comparison) {
+            if (self != null && self.EndsWith(suffix, comparison))
+                return self.Substring(0, self.Length - suffix.Length);
+            return self;
+        }
+
+        public static string RemoveAffixes(this string self, string affix) {
+            return RemoveAffixes(self, affix, StringComparison.CurrentCulture);
+        }
+
+        public static string RemoveAffixes(this string self, string affix, StringComparison comparison) {
+            if (self == null || self.Length < affix.Length)
+                return self;
+            var starts = self.StartsWith(affix, comparison);
+            var ends = self.EndsWith(affix, comparison);
+            if (starts & ends) {
+                if (String.Equals(self, affix, comparison))
+                    return "";
+                return self.Substring(affix.Length, self.Length - 2 * affix.Length);
+            } else if (starts) {
+                return self.Substring(affix.Length);
+            } else if (ends) {
+                return self.Substring(0, self.Length - affix.Length);
+            }
+            return self;
+        }
+
+        public static string RemoveAffixes(this string self, string prefix, string suffix) {
+            return RemoveAffixes(self, prefix, suffix, StringComparison.CurrentCulture);
+        }
+
+        public static string RemoveAffixes(this string self, string prefix, string suffix, StringComparison comparison) {
+            if (self == null)
+                return self;
+            var starts = self.StartsWith(prefix, comparison);
+            var ends = self.EndsWith(suffix, comparison);
+            if (starts & ends) {
+                if ((prefix.Length + suffix.Length) <= self.Length)
+                    return "";
+                return self.Substring(prefix.Length, self.Length - prefix.Length - -suffix.Length);
+            } else if (starts) {
+                return self.Substring(prefix.Length);
+            } else if (ends) {
+                return self.Substring(0, self.Length - suffix.Length);
+            }
+            return self;
+        }
+
+        #endregion
+
         #region ReplaceWord
 
         class WordReplacer
@@ -929,51 +995,6 @@ namespace WmcSoft
                     array[j++] = mapping[found].Value;
             }
             return new String(array, 0, j);
-        }
-
-        #endregion
-
-        #region RemovePrefix/RemoveSuffix/RemoveAffixes
-
-        public static string RemovePrefix(this string self, string prefix) {
-            return RemovePrefix(self, prefix, StringComparison.CurrentCulture);
-        }
-
-        public static string RemovePrefix(this string self, string prefix, StringComparison comparison) {
-            if (self != null && self.StartsWith(prefix, comparison))
-                return self.Substring(prefix.Length);
-            return self;
-        }
-
-        public static string RemoveSuffix(this string self, string suffix) {
-            return RemoveSuffix(self, suffix, StringComparison.CurrentCulture);
-        }
-
-        public static string RemoveSuffix(this string self, string suffix, StringComparison comparison) {
-            if (self != null && self.EndsWith(suffix, comparison))
-                return self.Substring(0, self.Length - suffix.Length);
-            return self;
-        }
-
-        public static string RemoveAffixes(this string self, string affix) {
-            return RemoveAffixes(self, affix, StringComparison.CurrentCulture);
-        }
-
-        public static string RemoveAffixes(this string self, string affix, StringComparison comparison) {
-            if (self == null || self.Length < affix.Length)
-                return self;
-            var starts = self.StartsWith(affix, comparison);
-            var ends = self.EndsWith(affix, comparison);
-            if (starts & ends) {
-                if (String.Equals(self, affix, comparison))
-                    return "";
-                return self.Substring(affix.Length, self.Length - 2 * affix.Length);
-            } else if (starts) {
-                return self.Substring(affix.Length);
-            } else if (ends) {
-                return self.Substring(0, self.Length - affix.Length);
-            }
-            return self;
         }
 
         #endregion
