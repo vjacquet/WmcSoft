d19ecef0954d1ae385dc0e7dd6a7b8f3bf461116
vjacquet <vjacquet@flowgroup.fr>
2016-03-05 18:09:35 +0100

    Added CopyOnWriteList

---
+++ WmcSoft.Core.Tests/Collections/Generic/CopyOnWriteListTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.Collections.Generic
+{
+    [TestClass]
+    public class CopyOnWriteListTests
+    {
+        [TestMethod]
+        public void CanAddToCopyOnWriteList() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            var cow = new CopyOnWriteList<int>(list);
+            cow.Add(6);
+
+            Assert.AreEqual(5, list.Count);
+            Assert.AreEqual(6, cow.Count);
+        }
+
+        [TestMethod]
+        public void CanRemoveToCopyOnWriteList() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            var cow = new CopyOnWriteList<int>(list);
+            cow.Remove(2);
+
+            Assert.AreEqual(5, list.Count);
+            Assert.IsTrue(list.Contains(2));
+            Assert.AreEqual(4, cow.Count);
+            Assert.IsFalse(cow.Contains(2));
+        }
+
+        [TestMethod]
+        public void CanRemoveAtToCopyOnWriteList() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            var cow = new CopyOnWriteList<int>(list);
+            cow.RemoveAt(1);
+
+            Assert.AreEqual(5, list.Count);
+            Assert.IsTrue(list.Contains(2));
+            Assert.AreEqual(4, cow.Count);
+            Assert.IsFalse(cow.Contains(2));
+        }
+
+        [TestMethod]
+        public void CanClearCopyOnWriteList() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            var cow = new CopyOnWriteList<int>(list);
+            cow.Clear();
+
+            Assert.AreEqual(5, list.Count);
+            Assert.AreEqual(0, cow.Count);
+        }
+
+        [TestMethod]
+        public void CanCopyFromCopyOnWriteList() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            var cow = new CopyOnWriteList<int>(list);
+            cow.Remove(2);
+            cow.Add(6);
+
+            var buffer = new int[5];
+
+            cow.CopyTo(buffer, 0);
+            CollectionAssert.AreEquivalent(buffer, new[] { 1, 3, 4, 5, 6 });
+
+            list.CopyTo(buffer);
+            CollectionAssert.AreEquivalent(buffer, new[] { 1, 2, 3, 4, 5 });
+        }
+
+        [TestMethod]
+        public void CanSetOrGetFromCopyOnWriteList() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            var cow = new CopyOnWriteList<int>(list);
+            cow[2] = 6;
+
+            Assert.AreEqual(6, cow[2]);
+            Assert.AreEqual(3, list[2]);
+        }
+    }
+}
---
+++ WmcSoft.Core/Collections/Generic/CopyOnWriteList.cs
@@ -0,0 +1,121 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WmcSoft.Collections.Generic
+{
+    public class CopyOnWriteList<T> : IList<T>
+    {
+        private readonly IList<T> _source;
+        private IList<T> _inner;
+
+        public CopyOnWriteList(IList<T> list) {
+            _inner = _source = list;
+        }
+
+        private IList<T> Readable
+        {
+            get {
+                return _inner;
+            }
+        }
+
+        private IList<T> Writtable
+        {
+            get {
+                if (ReferenceEquals(_inner, _source))
+                    _inner = new List<T>(_source);
+                return _inner;
+            }
+        }
+
+        public int Count
+        {
+            get {
+                return Readable.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get {
+                return false;
+            }
+        }
+
+        public T this[int index]
+        {
+            get {
+                return Readable[index];
+            }
+            set {
+                Writtable[index] = value;
+            }
+        }
+
+        public int IndexOf(T item) {
+            return Readable.IndexOf(item);
+        }
+
+        public void Insert(int index, T item) {
+            Writtable.Insert(index, item);
+        }
+
+        public void RemoveAt(int index) {
+            Writtable.RemoveAt(index);
+        }
+
+        public void Add(T item) {
+            Writtable.Add(item);
+        }
+
+        public void Clear() {
+            Writtable.Clear();
+        }
+
+        public bool Contains(T item) {
+            return Readable.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex) {
+            Readable.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item) {
+            return Writtable.Remove(item);
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            return Readable.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return Readable.GetEnumerator();
+        }
+    }
+}
