8a90a855e1f58c1d2e62efdc5725eb1faa7cd834
vjacquet <vjacquet@flowgroup.fr>
2016-03-06 08:21:45 +0100

    Added CopyOnWriteDictionary.

---
+++ WmcSoft.Core.Tests/Collections/Generic/CopyOnWriteDictionaryTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.Collections.Generic
+{
+    [TestClass]
+    public class CopyOnWriteDictionaryTests
+    {
+        [TestMethod]
+        public void CanAddToCopyOnWriteDictionary() {
+            var dictionary = new Dictionary<int, string>() { { 1, "one" }, { 2, "two" }, { 3, "three" }, { 4, "four" }, { 5, "five" } };
+            var cow = new CopyOnWriteDictionary<int, string>(dictionary);
+            cow.Add(6, "six");
+
+            Assert.AreEqual(5, dictionary.Count);
+            Assert.AreEqual(6, cow.Count);
+        }
+
+        [TestMethod]
+        public void CanRemoveToCopyOnWriteDictionary() {
+            var dictionary = new Dictionary<int, string>() { { 1, "one" }, { 2, "two" }, { 3, "three" }, { 4, "four" }, { 5, "five" } };
+            var cow = new CopyOnWriteDictionary<int, string>(dictionary);
+            cow.Remove(2);
+
+            Assert.AreEqual(5, dictionary.Count);
+            Assert.IsTrue(dictionary.ContainsKey(2));
+            Assert.AreEqual(4, cow.Count);
+            Assert.IsFalse(cow.ContainsKey(2));
+        }
+
+        [TestMethod]
+        public void CanClearCopyOnWriteDictionary() {
+            var dictionary = new Dictionary<int, string>() { { 1, "one" }, { 2, "two" }, { 3, "three" }, { 4, "four" }, { 5, "five" } };
+            var cow = new CopyOnWriteDictionary<int, string>(dictionary);
+            cow.Clear();
+
+            Assert.AreEqual(5, dictionary.Count);
+            Assert.AreEqual(0, cow.Count);
+        }
+
+        [TestMethod]
+        public void CanSetOrGetFromCopyOnWriteDictionary() {
+            var dictionary = new Dictionary<int, string>() { { 1, "one" }, { 2, "two" }, { 3, "three" }, { 4, "four" }, { 5, "five" } };
+            var cow = new CopyOnWriteDictionary<int, string>(dictionary);
+            cow[2] = "deux";
+
+            Assert.AreEqual("deux", cow[2]);
+            Assert.AreEqual("two", dictionary[2]);
+        }
+    }
+}
---
+++ WmcSoft.Core/Collections/Generic/CopyOnWriteDictionary.cs
@@ -0,0 +1,150 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WmcSoft.Collections.Generic
+{
+    public class CopyOnWriteDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        private readonly IDictionary<TKey, TValue> _source;
+        private IDictionary<TKey, TValue> _inner;
+        private readonly IEqualityComparer<TKey> _comparer;
+
+        public CopyOnWriteDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer) {
+            _inner = _source = dictionary;
+            _comparer = comparer;
+        }
+
+        public CopyOnWriteDictionary(IDictionary<TKey, TValue> dictionary)
+            : this(dictionary, EqualityComparer<TKey>.Default) {
+        }
+
+        public CopyOnWriteDictionary(Dictionary<TKey, TValue> dictionary)
+            : this(dictionary, dictionary.Comparer) {
+        }
+
+        private IDictionary<TKey, TValue> Readable
+        {
+            get {
+                return _inner;
+            }
+        }
+
+        private IDictionary<TKey, TValue> Writtable
+        {
+            get {
+                if (ReferenceEquals(_inner, _source))
+                    _inner = new Dictionary<TKey, TValue>(_source, _comparer);
+                return _inner;
+            }
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get {
+                return Readable.Keys;
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get {
+                return Readable.Values;
+            }
+        }
+
+        public int Count
+        {
+            get {
+                return Readable.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get {
+                return false;
+            }
+        }
+
+        public TValue this[TKey key]
+        {
+            get {
+                return Readable[key];
+            }
+
+            set {
+                Writtable[key] = value;
+            }
+        }
+
+        public bool ContainsKey(TKey key) {
+            return Readable.ContainsKey(key);
+        }
+
+        public void Add(TKey key, TValue value) {
+            Writtable.Add(key, value);
+        }
+
+        public bool Remove(TKey key) {
+            return Writtable.Remove(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value) {
+            return Readable.TryGetValue(key, out value);
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item) {
+            Writtable.Add(item);
+        }
+
+        public void Clear() {
+            Writtable.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item) {
+            return Readable.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) {
+            Readable.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item) {
+            return Writtable.Remove(item);
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
+            return Readable.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return Readable.GetEnumerator();
+        }
+    }
+}
