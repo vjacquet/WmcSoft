53fdd2d5d7c11282d3aa4d3d60ee84a82db4d901
vjacquet <vjacquet@flowgroup.fr>
2016-03-11 07:37:13 +0100

    Reformat the test code to make the result self evident.

--- WmcSoft.Windows.Forms.Tests/Windows/Forms/ControlExtensionsTests.cs
+++ WmcSoft.Windows.Forms.Tests/Windows/Forms/ControlExtensionsTests.cs
@@ -8,27 +8,29 @@ namespace WmcSoft.Windows.Forms
     [TestClass]
     public class ControlExtensionsTests
     {
+        Panel Build(string name, params Panel[] children) {
+            var panel = new Panel { Name = name };
+            panel.Controls.AddRange(children);
+            return panel;
+        }
+
         [TestMethod]
         public void CheckDescendantsOrSelf() {
-            var a = new Panel { Tag = "A" };
-            var b = new Panel { Tag = "B" };
-            var c = new Panel { Tag = "C" };
-            var d = new Panel { Tag = "D" };
-            var e = new Panel { Tag = "E" };
-            var f = new Panel { Tag = "F" };
-            var g = new Panel { Tag = "G" };
-            var h = new Panel { Tag = "H" };
-            var i = new Panel { Tag = "I" };
-            var j = new Panel { Tag = "J" };
-            var k = new Panel { Tag = "K" };
-
-            b.Controls.AddRange(new Control[] { c, d });
-            g.Controls.AddRange(new Control[] { h, i });
-            f.Controls.AddRange(new Control[] { g, j });
-            a.Controls.AddRange(new Control[] { b, e, f, k });
+            var root = Build("A",
+                Build("B",
+                    Build("C"),
+                    Build("D")),
+                Build("E"),
+                Build("F",
+                    Build("G",
+                        Build("H"),
+                        Build("I")),
+                    Build("J")),
+                Build("K")
+            );
 
             var expected = "ABCDEFGHIJK";
-            var actual = String.Join("", a.DescendantsAndSelf().Select(x => x.Tag));
+            var actual = String.Join("", root.DescendantsAndSelf().Select(x => x.Name));
             Assert.AreEqual(expected, actual);
         }
     }
