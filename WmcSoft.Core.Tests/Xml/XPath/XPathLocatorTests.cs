979539066109f7a22a71195ddaf38f2cfac4bf53
vjacquet <vjacquet@flowgroup.fr>
2016-04-15 18:10:27 +0200

    Refactor and add tests to XPathLocator and ObjectXPathNavigator.

--- WmcSoft.Core.Tests/Xml/XPath/XPathLocatorTests.cs
+++ WmcSoft.Core.Tests/Xml/XPath/XPathLocatorTests.cs
@@ -2,7 +2,6 @@
 using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WmcSoft.Properties;
-using WmcSoft.Xml;
 
 namespace WmcSoft.Xml.XPath
 {
@@ -18,39 +17,79 @@ namespace WmcSoft.Xml.XPath
         }
 
         [TestMethod]
-        public void CanLocateDocumentElement() {
-            string xpath = document.DocumentElement.GetXPath();
+        public void CanLocateDocument() {
+            var xpath = document.GetXPath();
+            Assert.AreEqual("/", xpath);
+        }
 
+        [TestMethod]
+        public void CanLocateDocumentElement() {
+            var xpath = document.DocumentElement.GetXPath();
             Assert.AreEqual("/rss", xpath);
         }
 
         [TestMethod]
         public void CanLocateAttribute() {
-            string xpath = document.DocumentElement.Attributes["version"].GetXPath();
-
+            var xpath = document.DocumentElement.Attributes["version"].GetXPath();
             Assert.AreEqual("/rss/@version", xpath);
         }
 
         [TestMethod]
         public void CanLocateXmlNS() {
-            XmlNode expected = document.DocumentElement.Attributes[1];
-
-            string xpath = expected.GetXPath();
-            XmlNode actual = document.SelectSingleNode(xpath);
+            var expected = document.DocumentElement.Attributes[1];
+            var xpath = expected.GetXPath();
+            var actual = document.SelectSingleNode(xpath);
 
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void CanLocateElement() {
-            XmlNode expected = document.SelectSingleNode(".//item[3]/*[4]");
-
-            string xpath = expected.GetXPath();
+            var expected = document.SelectSingleNode(".//item[3]/*[4]");
+            var xpath = expected.GetXPath();
 
             var nsmgr = new XmlNamespaceManager(document.NameTable);
-            string ns = document.NameTable.Get("dc");
+            var ns = document.NameTable.Get("dc");
             nsmgr.AddNamespace(ns, document.DocumentElement.GetNamespaceOfPrefix(ns));//@"http://purl.org/dc/elements/1.1/");
-            XmlNode actual = document.SelectSingleNode(xpath, nsmgr);
+            var actual = document.SelectSingleNode(xpath, nsmgr);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanLocateNamedElement() {
+            var expected = document.SelectSingleNode(".//item[3]/category[5]");
+            var xpath = expected.GetXPath();
+            var actual = document.SelectSingleNode(xpath);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanLocateProcessingInstruction() {
+            var expected = document.SelectSingleNode("/processing-instruction('xml-stylesheet')");
+            var xpath = expected.GetXPath();
+            var actual = document.SelectSingleNode(xpath);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanLocateTextElements() {
+            var sample = new XmlDocument();
+            sample.LoadXml("<root />");
+            sample.DocumentElement.AppendChild(sample.CreateTextNode("a"));
+            sample.DocumentElement.AppendChild(sample.CreateTextNode("b"));
+            sample.DocumentElement.AppendChild(sample.CreateElement("element"));
+            sample.DocumentElement.AppendChild(sample.CreateTextNode("c"));
+            sample.DocumentElement.AppendChild(sample.CreateElement("element"));
+            sample.DocumentElement.AppendChild(sample.CreateTextNode("d"));
+            sample.DocumentElement.AppendChild(sample.CreateTextNode("e"));
+            sample.DocumentElement.AppendChild(sample.CreateTextNode("f"));
+
+            var expected = sample.SelectSingleNode("/root/text()[3]");
+            var xpath = expected.GetXPath();
+            var actual = sample.SelectSingleNode(xpath);
 
             Assert.AreEqual(expected, actual);
         }
