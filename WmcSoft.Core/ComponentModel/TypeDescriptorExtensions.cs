0efe2ea7a75ec85dc994695d8ac5be812173fe7c
vjacquet <vjacquet@flowgroup.fr>
2016-04-20 08:45:23 +0200

    Added comments and improving code metrics.

--- WmcSoft.Core/ComponentModel/TypeDescriptorExtensions.cs
+++ WmcSoft.Core/ComponentModel/TypeDescriptorExtensions.cs
@@ -11,9 +11,9 @@ namespace WmcSoft.ComponentModel
     /// </summary>
     public static class TypeDescriptorExtensions
     {
-        static object ResolveValue(PropertyDescriptor property, object value) {
+        static object ResolveValue(Type target, object value) {
             var type = value as Type;
-            if (type != null && property.PropertyType.IsAssignableFrom(type)) {
+            if (type != null && target.IsAssignableFrom(type)) {
                 return Activator.CreateInstance(type);
             }
             return value;
@@ -23,23 +23,34 @@ namespace WmcSoft.ComponentModel
         /// Sets the values of the component.
         /// </summary>
         /// <param name="properties">The property descriptor collection.</param>
-        /// <param name="component">The component whose properties must be set.</param>
+        /// <param name="component">The component whose properties to be set.</param>
         /// <param name="values">The values to set.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="values"/> is null.</exception>
         public static void SetValues(this PropertyDescriptorCollection properties, object component, IDictionary<string, object> values) {
-            if (values == null || component == null)
-                return;
+            if (values == null) throw new ArgumentNullException("values");
 
             foreach (var entry in values) {
                 var property = properties[entry.Key];
                 if (property != null) {
-                    var value = ResolveValue(property, entry.Value);
+                    var value = ResolveValue(property.PropertyType, entry.Value);
                     property.SetValue(component, value);
                 }
             }
         }
 
+        /// <summary>
+        /// Gets the values of the component.
+        /// </summary>
+        /// <param name="properties"></param>
+        /// <param name="component"></param>
+        /// <param name="properties">The property descriptor collection.</param>
+        /// <param name="component">The component whose properties to get.</param>
+        /// <param name="values">The values to get.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="values"/> is null.</exception>
         public static void GetValues(this PropertyDescriptorCollection properties, object component, IDictionary<string, object> values) {
-            foreach (var key in values.Keys.ToList()) {
+            if (values == null) throw new ArgumentNullException("values");
+
+            foreach (var key in values.Keys.ToArray()) {
                 var property = properties[key];
                 if (property != null) {
                     values[key] = property.GetValue(component);
@@ -55,17 +66,23 @@ namespace WmcSoft.ComponentModel
         /// <param name="value">When this method returns, contains the value of the property, if the property descriptor or the component are not null; otherwhise, null.</param>
         /// <returns>Returns false when the property or the component are null.</returns>
         public static bool TryGetValue(this PropertyDescriptor property, object component, out object value) {
-            if (property != null && component != null) {
-                value = property.GetValue(component);
-                return true;
+            if (property == null) {
+                value = null;
+                return false;
             }
-            value = null;
-            return false;
+            value = property.GetValue(component);
+            return true;
         }
 
-        public static TAttribute GetMetadataAttribute<TAttribute>(this PropertyDescriptor propertyDescriptor)
+        /// <summary>
+        /// Gets the attribute of the specified type for this member.
+        /// </summary>
+        /// <typeparam name="TAttribute">The type of the attribute.</typeparam>
+        /// <param name="descriptor">The descriptor of the member.</param>
+        /// <returns>The attribute.</returns>
+        public static TAttribute GetMetadataAttribute<TAttribute>(this MemberDescriptor descriptor)
             where TAttribute : Attribute {
-            return propertyDescriptor.Attributes[typeof(TAttribute)] as TAttribute;
+            return (TAttribute)descriptor.Attributes[typeof(TAttribute)];
         }
     }
 }
