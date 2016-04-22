f6bfb2cb7b30c39d4525d8500ac8ab6aad470d44
vjacquet <vjacquet@flowgroup.fr>
2016-04-22 07:03:55 +0200

    Simplified ICustomAttributeProvider extensions.

--- WmcSoft.Core/Reflection/CustomAttributeProviderExtensions.cs
+++ WmcSoft.Core/Reflection/CustomAttributeProviderExtensions.cs
@@ -30,45 +30,45 @@ using System.Reflection;
 
 namespace WmcSoft.Reflection
 {
+    /// <summary>
+    /// Defines extension methods to the <see cref="ICustomAttributeProvider"/> interface.
+    /// This is a static class.
+    /// </summary>
     public static class CustomAttributeProviderExtensions
     {
         /// <summary>
         /// Returns an array of custom attributes defined on this provider.
         /// </summary>
-        /// <typeparam name="T">The type of the custom attribute</typeparam>
+        /// <typeparam name="TAttribute">The type of the custom attribute</typeparam>
         /// <param name="self">The custom attribute provider</param>
         /// <param name="inherit">When true, lookup the hierarchy chain for the inherited custom attribute</param>
         /// <returns>The array of the custom attributes, or an empty array if not found.</returns>
-        public static T[] GetCustomAttributes<T>(this ICustomAttributeProvider self, bool inherit)
-            where T : Attribute {
-            object[] result = self.GetCustomAttributes(typeof(T), inherit);
-            T[] array = result as T[];
-            if (array != null)
-                return array;
-            return result.OfType<T>().ToArray();
+        public static TAttribute[] GetCustomAttributes<TAttribute>(this ICustomAttributeProvider self, bool inherit)
+            where TAttribute : Attribute {
+            return (TAttribute[])self.GetCustomAttributes(typeof(TAttribute), inherit);
         }
 
         /// <summary>
         /// Returns the first custom attributes defined on this provider, or default.
         /// </summary>
-        /// <typeparam name="T">The type of the custom attribute</typeparam>
+        /// <typeparam name="TAttribute">The type of the custom attribute</typeparam>
         /// <param name="self">The custom attribute provider</param>
         /// <param name="inherit">When true, lookup the hierarchy chain for the inherited custom attribute</param>
         /// <returns></returns>
-        public static T GetCustomAttribute<T>(this ICustomAttributeProvider self, bool inherit)
-            where T : Attribute {
-            return GetCustomAttributes<T>(self, inherit).FirstOrDefault();
+        public static TAttribute GetCustomAttribute<TAttribute>(this ICustomAttributeProvider self, bool inherit)
+            where TAttribute : Attribute {
+            return GetCustomAttributes<TAttribute>(self, inherit).FirstOrDefault();
         }
 
         /// <summary>
         /// Indicate wether one or more instance of the attribute is defined on this provider.
-        /// <typeparam name="T">The type of the custom attribute</typeparam>
+        /// <typeparam name="TAttribute">The type of the custom attribute</typeparam>
         /// <param name="self">The custom attribute provider</param>
         /// <param name="inherit">When true, lookup the hierarchy chain for the inherited custom attribute</param>
         /// <returns>true if the custom attribute is defined</returns>
-        public static bool IsDefined<T>(this ICustomAttributeProvider self, bool inherit)
-            where T : Attribute {
-            return self.IsDefined(typeof(T), inherit);
+        public static bool IsDefined<TAttribute>(this ICustomAttributeProvider self, bool inherit)
+            where TAttribute : Attribute {
+            return self.IsDefined(typeof(TAttribute), inherit);
         }
     }
 }
