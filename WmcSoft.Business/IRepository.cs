93900437e9372100e93ac0ba0302034068d22451
vjacquet <vjacquet@flowgroup.fr>
2016-05-13 10:38:42 +0200

    Cleanup.

--- WmcSoft.Business/IRepository.cs
+++ WmcSoft.Business/IRepository.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,157 +41,3 @@ namespace WmcSoft.Business
         IQueryable<TValue> Items { get; }
     }
 }
-
-
-
-
-
-
-    /// <summary>
-    /// Utility class to store values and and keep track of wether it changed or not.
-    /// </summary>
-    /// <typeparam name="T">The type of the value to store.</typeparam>
-    /// <remarks>Think of a better name</remarks>
-    [DebuggerDisplay("{_value,nq}{_changed==1 ? \"*\" : \"\",nq}")]
-    public class Publishable<T>
-    {
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private T _value;
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private int _changed;
-        private readonly IEqualityComparer<T> _comparer;
-
-        public Publishable() {
-            _comparer = EqualityComparer<T>.Default;
-        }
-
-        public Publishable(T value, IEqualityComparer<T> comparer) {
-            _comparer = comparer;
-            _value = value;
-        }
-
-        public Publishable(IEqualityComparer<T> comparer) {
-            _comparer = comparer;
-        }
-
-        public Publishable(T value)
-            : this() {
-            _value = value;
-        }
-
-        /// <summary>
-        /// Updates the stored value
-        /// </summary>
-        /// <param name="value">The value</param>
-        /// <returns>Returns true if the value has changed.</returns>
-        /// <remarks>We always keep the most recent value, wether it changed or not.
-        /// It maintenains the lifetime of references short.</remarks>
-        public bool Update(T value) {
-            T current = _value;
-            _value = value;
-            if (_changed == 0 && !_comparer.Equals(current, value))
-                _changed = 1;
-            return _changed != 0;
-        }
-
-        /// <summary>
-        /// Returns the stored value and a boolean value indicated if it changed or not since last time.
-        /// </summary>
-        /// <param name="value">The value</param>
-        /// <returns>The dirty flag</returns>
-        public bool TryGetChanges(out T value) {
-            value = _value;
-            var changed = Interlocked.CompareExchange(ref _changed, 0, 1);
-            return changed == 1;
-        }
-
-        /// <summary>
-        /// Forces the dirty flag to true.
-        /// </summary>
-        public void MarkAsDirty() {
-            _changed = 1;
-        }
-
-        /// <summary>
-        /// The value
-        /// </summary>
-        public T Value {
-            get {
-                return _value;
-            }
-        }
-
-        /// <summary>
-        /// Implicit cast to get the stored value.
-        /// </summary>
-        /// <param name="self">The instance</param>
-        /// <returns>Its stored value.</returns>
-        public static implicit operator T(Publishable<T> self) {
-            return self.Value;
-        }
-    }
-
-    public static class ChangeWatcherExtensions
-    {
-        /// <summary>
-        /// Gets the spot value of a publishable over an array.
-        /// </summary>
-        /// <typeparam name="T">The type of the items in the array</typeparam>
-        /// <param name="self">The publishable</param>
-        /// <param name="defaultValue">The default value to return when the array is empty</param>
-        /// <returns>The spot value or default(T) when the array is empty.</returns>
-        /// <remarks>Gets the value once</remarks>
-        public static T Spot<T>(this Publishable<T[]> self, T defaultValue = default(T)) {
-            var value = self.Value;
-            if (value.Length > 0)
-                return value[0];
-            return defaultValue;
-        }
-
-        /// <summary>
-        /// Applies a function on the spot value of a publishable over an array.
-        /// </summary>
-        /// <typeparam name="T">The type of the items in the array</typeparam>
-        /// <typeparam name="TResult">The return type of the function to apply</typeparam>
-        /// <param name="self">The publishable</param>
-        /// <param name="selector">The function to apply</param>
-        /// <param name="defaultValue">The default value to return when the array is empty</param>
-        /// <returns></returns>
-        /// <remarks>Gets the value once</remarks>
-        public static TResult Spot<T, TResult>(this Publishable<T[]> self, Func<T, TResult> selector, TResult defaultValue = default(TResult)) {
-            var value = self.Value;
-            if (value.Length > 0)
-                return selector(value[0]);
-            return defaultValue;
-        }
-
-        /// <summary>
-        /// Gets the spot value of an array.
-        /// </summary>
-        /// <typeparam name="T">The type of the items in the array</typeparam>
-        /// <param name="value">The array</param>
-        /// <param name="defaultValue">The default value to return when the array is empty</param>
-        /// <returns>The spot value or default(T) when the array is empty.</returns>
-        /// <remarks>Gets the value once</remarks>
-        public static T Spot<T>(this T[] value, T defaultValue = default(T)) {
-            if (value.Length > 0)
-                return value[0];
-            return defaultValue;
-        }
-
-        /// <summary>
-        /// Applies a function on the spot value of an array.
-        /// </summary>
-        /// <typeparam name="T">The type of the items in the array</typeparam>
-        /// <typeparam name="TResult">The return type of the function to apply</typeparam>
-        /// <param name="value">The array</param>
-        /// <param name="selector">The function to apply</param>
-        /// <param name="defaultValue">The default value to return when the array is empty</param>
-        /// <returns></returns>
-        /// <remarks>Gets the value once</remarks>
-        public static TResult Spot<T, TResult>(this T[] value, Func<T, TResult> selector, TResult defaultValue = default(TResult)) {
-            if (value.Length > 0)
-                return selector(value[0]);
-            return defaultValue;
-        }
-    }
