1c0a3937b6f7f3536350970e9c11b4bae52c6b15
vjacquet <vjacquet@flowgroup.fr>
2016-05-17 05:16:13 +0200

    Added checkpoints.

---
+++ WmcSoft.Core/Diagnostics/Checkpoints/CheckpointBase.cs
@@ -0,0 +1,130 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.ComponentModel;
+
+namespace WmcSoft.Diagnostics.Checkpoints
+{
+    /// <summary>
+    /// Base implementation of a <see cref="Checkpoint"/>.
+    /// </summary>
+    public abstract class CheckpointBase : ICheckpoint
+    {
+        protected CheckpointBase(string name) {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public virtual int MinimumLevel { get { return 0; } }
+
+        public CheckpointResult Verify(int level) {
+            try {
+                return DoVerify(level);
+            }
+            catch (WarningException exception) {
+                return Warn(exception.Message);
+            }
+            catch (Exception exception) {
+                return Error(exception.Message);
+            }
+        }
+
+        protected abstract CheckpointResult DoVerify(int level);
+
+        #region Helpers
+
+        protected CheckpointResult Success() {
+            return new CheckpointResult(CheckpointResultType.Success, null);
+        }
+
+        protected CheckpointResult Failed() {
+            return new CheckpointResult(CheckpointResultType.Failed, null);
+        }
+
+        protected CheckpointResult Info(string message) {
+            return new CheckpointResult(CheckpointResultType.Info, message);
+        }
+
+        protected CheckpointResult Info(string format, object arg0) {
+            return new CheckpointResult(CheckpointResultType.Info, String.Format(format, arg0));
+        }
+
+        protected CheckpointResult Info(string format, object arg0, object arg1) {
+            return new CheckpointResult(CheckpointResultType.Info, String.Format(format, arg0, arg1));
+        }
+
+        protected CheckpointResult Info(string format, object arg0, object arg1, object arg2) {
+            return new CheckpointResult(CheckpointResultType.Info, String.Format(format, arg0, arg1, arg2));
+        }
+
+        protected CheckpointResult Info(string format, params object[] args) {
+            return new CheckpointResult(CheckpointResultType.Info, String.Format(format, args));
+        }
+
+        protected CheckpointResult Warn(string message) {
+            return new CheckpointResult(CheckpointResultType.Warn, message);
+        }
+
+        protected CheckpointResult Warn(string format, object arg0) {
+            return new CheckpointResult(CheckpointResultType.Warn, String.Format(format, arg0));
+        }
+
+        protected CheckpointResult Warn(string format, object arg0, object arg1) {
+            return new CheckpointResult(CheckpointResultType.Warn, String.Format(format, arg0, arg1));
+        }
+
+        protected CheckpointResult Warn(string format, object arg0, object arg1, object arg2) {
+            return new CheckpointResult(CheckpointResultType.Warn, String.Format(format, arg0, arg1, arg2));
+        }
+
+        protected CheckpointResult Warn(string format, params object[] args) {
+            return new CheckpointResult(CheckpointResultType.Info, String.Format(format, args));
+        }
+
+        protected CheckpointResult Error(string message) {
+            return new CheckpointResult(CheckpointResultType.Error, message);
+        }
+
+        protected CheckpointResult Error(string format, object arg0) {
+            return new CheckpointResult(CheckpointResultType.Error, String.Format(format, arg0));
+        }
+
+        protected CheckpointResult Error(string format, object arg0, object arg1) {
+            return new CheckpointResult(CheckpointResultType.Error, String.Format(format, arg0, arg1));
+        }
+
+        protected CheckpointResult Error(string format, object arg0, object arg1, object arg2) {
+            return new CheckpointResult(CheckpointResultType.Info, String.Format(format, arg0, arg1, arg2));
+        }
+
+        protected CheckpointResult Error(string format, params object[] args) {
+            return new CheckpointResult(CheckpointResultType.Error, String.Format(format, args));
+        }
+
+        #endregion
+    }
+}
---
+++ WmcSoft.Core/Diagnostics/Checkpoints/CheckpointResult.cs
@@ -0,0 +1,94 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace WmcSoft.Diagnostics.Checkpoints
+{
+    /// <summary>
+    /// Checkpoint's result builder.
+    /// </summary>
+    public class CheckpointResult
+    {
+        private readonly List<string> _lines;
+
+        public CheckpointResult(CheckpointResultType resultType, string message) {
+            ResultType = resultType;
+            _lines = new List<string> { message };
+            if (!string.IsNullOrEmpty(message))
+                _lines.Add(message);
+        }
+
+        public CheckpointResult(string message)
+            : this(CheckpointResultType.Info, message) {
+        }
+
+        public CheckpointResultType ResultType { get; private set; }
+        public IReadOnlyCollection<string> Lines { get { return _lines; } }
+
+        /// <summary>
+        /// Sets the result type only if it has a stronger meaning.
+        /// </summary>
+        /// <param name="resultType">The new result type</param>
+        /// <return>The new result type</return>
+        public CheckpointResult OverrideResultType(CheckpointResultType resultType) {
+            if ((int)resultType > (int)ResultType)
+                ResultType = resultType;
+            return this;
+        }
+
+        protected virtual void DoWriteLine(object line) {
+            if (line != null)
+                _lines.Add(line.ToString());
+        }
+
+        public CheckpointResult WriteLine(string line) {
+            DoWriteLine(line);
+            return this;
+        }
+
+        public CheckpointResult WriteLine(object line) {
+            return WriteLine(line);
+        }
+
+        public CheckpointResult WriteLine(IFormatProvider provider, string format, params object[] args) {
+            return WriteLine(string.Format(provider, format, args));
+        }
+        public CheckpointResult WriteLine(string format, object arg0) {
+            return WriteLine(string.Format(format, arg0));
+        }
+        public CheckpointResult WriteLine(string format, object arg0, object arg1) {
+            return WriteLine(string.Format(format, arg0, arg1));
+        }
+        public CheckpointResult WriteLine(string format, object arg0, object arg1, object arg2) {
+            return WriteLine(string.Format(format, arg0, arg1, arg2));
+        }
+        public CheckpointResult WriteLine(string format, params object[] args) {
+            return WriteLine(string.Format(format, args));
+        }
+    }
+}
