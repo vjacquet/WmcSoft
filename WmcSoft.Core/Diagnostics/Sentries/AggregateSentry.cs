4a02d83ad63e2fc6d4c515db7d64dece3e9f3863
vjacquet <vjacquet@flowgroup.fr>
2016-05-19 05:32:46 +0200

    Added AggregatedSentry and TraceSentry.

---
+++ WmcSoft.Core/Diagnostics/Sentries/AggregateSentry.cs
@@ -0,0 +1,119 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Linq;
+
+namespace WmcSoft.Diagnostics.Sentries
+{
+    public class AggregateSentry : SentryBase, IObserver<SentryStatus>, IDisposable
+    {
+        #region Utility classes
+
+        sealed class SentryStatusAggregator
+        {
+            private int _aggregated;
+
+            public SentryStatusAggregator Add(ISentry sentry) {
+                return Add(sentry.Status);
+            }
+            public SentryStatusAggregator Add(SentryStatus value) {
+                switch (value) {
+                case SentryStatus.Success:
+                    _aggregated |= 1;
+                    break;
+                case SentryStatus.Warning:
+                    _aggregated |= 3;
+                    break;
+                case SentryStatus.Error:
+                    _aggregated |= 2;
+                    break;
+                }
+                return this;
+            }
+
+            public SentryStatus GetResult() {
+                switch (_aggregated) {
+                case 1:
+                    return SentryStatus.Success;
+                case 2:
+                    return SentryStatus.Error;
+                case 3:
+                    return SentryStatus.Warning;
+                case 0:
+                default:
+                    return SentryStatus.None;
+                }
+            }
+        }
+
+        #endregion
+
+        private readonly ISentry[] _sentries;
+        private readonly IDisposable[] _unsubscribers;
+
+        public AggregateSentry(string name, params ISentry[] sentries) : base(name) {
+            _sentries = sentries;
+            _unsubscribers = new IDisposable[_sentries.Length];
+        }
+
+        protected override void OnObserving() {
+            var length = _sentries.Length;
+            for (int i = 0; i < length; i++) {
+                _unsubscribers[i] = _sentries[i].Subscribe(this);
+            }
+        }
+
+        protected override void OnObserved() {
+            UnsubscribeAll();
+        }
+
+        void IObserver<SentryStatus>.OnNext(SentryStatus value) {
+            var aggregator = new SentryStatusAggregator();
+            var aggregate = _sentries.Aggregate(aggregator, (a, s) => a.Add(s)).GetResult();
+            OnNext(aggregate);
+        }
+
+        void IObserver<SentryStatus>.OnError(Exception error) {
+        }
+
+        void IObserver<SentryStatus>.OnCompleted() {
+        }
+
+        public void Dispose() {
+            UnsubscribeAll();
+        }
+
+        private void UnsubscribeAll() {
+            var length = _sentries.Length;
+            for (int i = 0; i < length; i++) {
+                var disposer = _unsubscribers[i];
+                _unsubscribers[i] = null;
+                disposer?.Dispose();
+            }
+        }
+    }
+}
---
+++ WmcSoft.Core/Diagnostics/Sentries/TraceSentry.cs
@@ -0,0 +1,116 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Diagnostics;
+
+namespace WmcSoft.Diagnostics.Sentries
+{
+    public class TraceSentry : ISentry
+    {
+        #region Utility classes
+
+        class Tracer : IObserver<SentryStatus>
+        {
+            private readonly TraceSentry _owner;
+
+            public Tracer(TraceSentry owner) {
+                _owner = owner;
+            }
+
+            public void OnNext(SentryStatus value) {
+                _owner.TraceNext(value);
+            }
+
+            public void OnCompleted() {
+                _owner.TraceCompleted();
+            }
+
+            public void OnError(Exception error) {
+                _owner.TraceError(error);
+            }
+        }
+
+        #endregion
+
+        private readonly TraceSource _traceSource;
+        private readonly ISentry _decorated;
+        private readonly object _syncRoot;
+        private int _refCount;
+        private IDisposable _unsubscriber;
+
+        public TraceSentry(TraceSource traceSource, ISentry decorated) {
+            _syncRoot = new Object();
+            _traceSource = traceSource;
+            _decorated = decorated;
+            _refCount = 0;
+        }
+
+        public string Name {
+            get { return _decorated.Name; }
+        }
+
+        public SentryStatus Status {
+            get { return _decorated.Status; }
+        }
+
+        public IDisposable Subscribe(IObserver<SentryStatus> observer) {
+            lock (_syncRoot) {
+                if (++_refCount == 1) {
+                    _unsubscriber = _decorated.Subscribe(new Tracer(this));
+                }
+            }
+            var unsubscriber = _decorated.Subscribe(observer);
+            return new Disposer(() => {
+                unsubscriber.Dispose();
+
+                var dispose = false;
+                IDisposable disposer = _unsubscriber;
+                lock (_syncRoot) {
+                    if (--_refCount == 0) {
+                        dispose = true;
+                        _unsubscriber = null;
+                    }
+                }
+                if (dispose) {
+                    disposer.Dispose();
+                }
+            });
+        }
+
+        private void TraceNext(SentryStatus value) {
+            _traceSource.TraceInformation("Sentry {0} observed {1}.", Name, value);
+        }
+
+        private void TraceCompleted() {
+            _traceSource.TraceInformation("Sentry {0} completed.", Name);
+        }
+
+        private void TraceError(Exception error) {
+            _traceSource.TraceError(0, "Sentry {0} failed: {1}.", Name, error);
+        }
+    }
+}
