250eccd7c0d315adfb969a7594e75fc473fb4277
vjacquet <vjacquet@flowgroup.fr>
2016-05-20 06:27:02 +0200

    Added Action & Predicate checkpoints.

---
+++ WmcSoft.Core/Diagnostics/Checkpoints/ActionCheckpoint.cs
@@ -0,0 +1,57 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+
+namespace WmcSoft.Diagnostics.Checkpoints
+{
+    /// <summary>
+    /// Checkpoint based on an Action. 
+    /// </summary>
+    /// <remarks>Throw exceptions to notify warnings or errors.</remarks>
+    public sealed class ActionCheckpoint : CheckpointBase
+    {
+        private readonly Action _action;
+        private readonly int _minimumLevel;
+
+        public ActionCheckpoint(string name, Action action, int minimumLevel = 0)
+            : base(name) {
+            if (action == null) throw new ArgumentNullException("action");
+
+            _action = action;
+            _minimumLevel = minimumLevel;
+        }
+
+        public override int MinimumLevel {
+            get { return _minimumLevel; }
+        }
+
+        protected override CheckpointResult DoVerify(int level) {
+            _action();
+            return Success();
+        }
+    }
+}
---
+++ WmcSoft.Core/Diagnostics/Checkpoints/PredicateCheckpoint.cs
@@ -0,0 +1,58 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+
+namespace WmcSoft.Diagnostics.Checkpoints
+{
+    /// <summary>
+    /// Checkpoint based on a Predicate. 
+    /// </summary>
+    /// <remarks>Throw exceptions to notify warnings or errors.</remarks>
+    public sealed class PredicateCheckpoint : CheckpointBase
+    {
+        private readonly Func<bool> _predicate;
+        private readonly int _minimumLevel;
+
+        public PredicateCheckpoint(string name, Func<bool> predicate, int minimumLevel = 0)
+            : base(name) {
+            if (predicate == null) throw new ArgumentNullException("predicate");
+
+            _predicate = predicate;
+            _minimumLevel = minimumLevel;
+        }
+
+        public override int MinimumLevel {
+            get { return _minimumLevel; }
+        }
+
+        protected override CheckpointResult DoVerify(int level) {
+            if (_predicate())
+                return Success();
+            return Failed();
+        }
+    }
+}
