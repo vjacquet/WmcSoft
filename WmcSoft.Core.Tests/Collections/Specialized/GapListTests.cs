02be81be42ee5276e77aaad3b8edb825cd69d19b
vjacquet <vjacquet@flowgroup.fr>
2016-06-01 12:28:44 +0200

    Added a gap list.

---
+++ WmcSoft.Core.Tests/Collections/Specialized/GapListTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.Collections.Specialized
+{
+    [TestClass]
+    public class GapListTests
+    {
+        [TestMethod]
+        public void CanInsert() {
+            var list = new GapList<int>(10);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Insert(4, 9);
+            list.Add(10);
+            list.Insert(7, 7);
+            list.Add(8);
+            var expected = new[] { 1, 2, 3, 4, 9, 10, 5, 7, 8, 6 };
+            var actual = list.ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanInsertWithoutMoving() {
+            var list = new GapList<int>(5);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Insert(4, 9);
+            list.Add(10);
+            list.Insert(6, 7);
+            list.Add(8);
+            var expected = list.ToArray();
+
+            list = new GapList<int>(5);
+            list.Clear();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Insert(4, 9);
+            list.Add(10);
+            list.Add(7);
+            list.Add(8);
+
+            var actual = list.ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanRemove() {
+            var list = new GapList<int>(5);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Insert(4, 9);
+            list.Add(10);
+            list.Insert(7, 7);
+            list.Add(8);
+            list.Remove(3);
+            list.Remove(10);
+
+            var expected = new[] { 1, 2, 4, 9, 5, 7, 8, 6 };
+            var actual = list.ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ WmcSoft.Core/Collections/Specialized/GapList.cs
@@ -0,0 +1,219 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WmcSoft.Collections.Specialized
+{
+    public sealed class GapList<T> : IList<T>
+    {
+        // *************........................*******
+        // ^            ^                       ^      ^
+        // 0     _gapStartIndex                 |     capacity
+        //                               _gapEndIndex
+
+        private static readonly T[] Empty = new T[0];
+        private const int DefaultCapacity = 4;
+
+        private T[] _storage;
+        //private int _count;
+        //private int _gapIndex;
+
+        private int _gapStartIndex;
+        private int _gapEndIndex;
+
+        public GapList() {
+            _storage = Empty;
+        }
+
+        public GapList(int capacity) {
+            if (capacity < 0) throw new ArgumentOutOfRangeException("capacity");
+            _storage = capacity == 0 ? Empty : new T[capacity];
+            _gapEndIndex = capacity;
+        }
+
+        public GapList(IEnumerable<T> collection) {
+            if (collection == null) throw new ArgumentOutOfRangeException("collection");
+
+            var c = collection as ICollection<T>;
+            if (c != null) {
+                var count = c.Count;
+                if (count == 0) {
+                    _storage = Empty;
+                } else {
+                    _storage = new T[count];
+                    c.CopyTo(_storage, 0);
+                    _gapStartIndex = count;
+                    _gapEndIndex = count;
+                }
+            } else {
+                _storage = Empty;
+                using (IEnumerator<T> enumerator = collection.GetEnumerator()) {
+                    while (enumerator.MoveNext()) {
+                        Add(enumerator.Current);
+                    }
+                }
+            }
+        }
+
+        private int Translate(int index) {
+            if (index < _gapStartIndex)
+                return index;
+            return index - _gapStartIndex + _gapEndIndex;
+        }
+
+        void Reserve(int grow) {
+            var gap = _gapEndIndex - _gapStartIndex;
+            if (gap < grow) {
+                Capacity = Math.Max(Count + gap, _storage.Length == 0 ? DefaultCapacity : _storage.Length * 2);
+            }
+        }
+
+        void Seek(int position) {
+            if (position < _gapStartIndex) {
+                // *****-----.....*****
+                //      ^
+                // *****.....-----*****
+                var n = _gapStartIndex - position;
+                _gapEndIndex = _storage.Rotate(-n, position, _gapEndIndex - position);
+                _gapStartIndex = position;
+            } else if (position > _gapStartIndex) {
+                // ***********.....******
+                //                    ^
+                // **************.....***
+                var n = position - _gapStartIndex;
+                var gap = _gapEndIndex - _gapStartIndex;
+                _gapEndIndex = _storage.Rotate(n, _gapStartIndex, n + gap) + gap;
+                _gapStartIndex = position;
+            }
+        }
+
+        public int Capacity {
+            get {
+                return _storage.Length;
+            }
+            set {
+                var count = Count;
+                if (value < count) throw new ArgumentOutOfRangeException("value");
+
+                if (value != _storage.Length) {
+                    if (value > 0) {
+                        T[] storage = new T[value];
+                        if (count > 0) {
+                            Array.Copy(_storage, 0, storage, 0, _gapStartIndex);
+                            var n = _storage.Length - _gapEndIndex;
+                            Array.Copy(_storage, _gapEndIndex, storage, storage.Length - n, n);
+                            _gapEndIndex = storage.Length - n;
+                        }
+                        _storage = storage;
+                    } else {
+                        _storage = Empty;
+                        _gapEndIndex = 0;
+                    }
+                }
+            }
+        }
+
+        public T this[int index] {
+            get { return _storage[Translate(index)]; }
+            set { _storage[Translate(index)] = value; }
+        }
+
+        public int Count {
+            get { return _gapStartIndex + _storage.Length - _gapEndIndex; }
+        }
+
+        public bool IsReadOnly {
+            get { return false; }
+        }
+
+        public void Add(T item) {
+            Reserve(1);
+            _storage[_gapStartIndex++] = item;
+        }
+
+        public void Clear() {
+            _gapStartIndex = 0;
+            _gapEndIndex = _storage.Length;
+        }
+
+        public bool Contains(T item) {
+            return IndexOf(item) >= 0;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex) {
+            Array.Copy(_storage, 0, array, arrayIndex, _gapStartIndex);
+            Array.Copy(_storage, _gapEndIndex, array, arrayIndex + _gapStartIndex, _storage.Length - _gapEndIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            using (var enumerator = _storage.GetEnumerator(0, _gapStartIndex)) {
+                while (enumerator.MoveNext())
+                    yield return enumerator.Current;
+            }
+
+            using (var enumerator = _storage.GetEnumerator(_gapEndIndex, _storage.Length - _gapEndIndex)) {
+                while (enumerator.MoveNext())
+                    yield return enumerator.Current;
+            }
+        }
+
+        public int IndexOf(T item) {
+            var found = Array.IndexOf(_storage, item, 0, _gapStartIndex);
+            if (found >= 0)
+                return found;
+            var gap = _gapEndIndex - _gapStartIndex;
+            return Array.IndexOf(_storage, item, _gapEndIndex, _storage.Length - _gapEndIndex) - gap;
+        }
+
+        public void Insert(int index, T item) {
+            Seek(index);
+            Add(item);
+        }
+
+        public bool Remove(T item) {
+            var found = IndexOf(item);
+            if (found < 0)
+                return false;
+            RemoveAt(found);
+            return true;
+        }
+
+        public void RemoveAt(int index) {
+            Seek(index + 1);
+            _gapStartIndex--;
+#if DEBUG
+            _storage[_gapStartIndex] = default(T);
+#endif
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+}
