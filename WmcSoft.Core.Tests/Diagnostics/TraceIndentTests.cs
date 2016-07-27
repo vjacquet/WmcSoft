32bdf7a028c2fc29c2ebe678f9c45aa8d8ed8b6c
vjacquet <vjacquet@flowgroup.fr>
2016-07-27 21:02:18 +0200

    Added tests for TraceIndent.

---
+++ WmcSoft.Core.Tests/Diagnostics/TraceIndentTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.Diagnostics
+{
+    [TestClass]
+    public class TraceIndentTests
+    {
+
+        [TestMethod]
+        public void CanIndentTrace() {
+            var indent = Trace.IndentLevel;
+            using (new TraceIndent()) {
+                Assert.IsTrue(indent < Trace.IndentLevel);
+            }
+                Assert.AreEqual(indent, Trace.IndentLevel);
+        }
+    }
+}
