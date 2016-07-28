3d639f6b2e3bb1a4665a21caff8b1471f63bf6d5
vjacquet <vjacquet@flowgroup.fr>
2016-07-28 15:04:37 +0200

    Reformat.

--- WmcSoft.Core.Tests/Diagnostics/TraceIndentTests.cs
+++ WmcSoft.Core.Tests/Diagnostics/TraceIndentTests.cs
@@ -6,14 +6,13 @@ namespace WmcSoft.Diagnostics
     [TestClass]
     public class TraceIndentTests
     {
-
         [TestMethod]
         public void CanIndentTrace() {
             var indent = Trace.IndentLevel;
             using (new TraceIndent()) {
                 Assert.IsTrue(indent < Trace.IndentLevel);
             }
-                Assert.AreEqual(indent, Trace.IndentLevel);
+            Assert.AreEqual(indent, Trace.IndentLevel);
         }
     }
 }
