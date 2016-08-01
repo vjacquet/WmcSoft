50b09b7c5480cdd8497133ddc9c29edb11e8657c
vjacquet <vjacquet@flowgroup.fr>
2016-08-01 20:20:55 +0200

    Added CombineWith and MergeWith extensions on IDictionary<TKey,TValue>.

--- WmcSoft.Core/Collections/Generic/DictionaryExtensions.cs
+++ WmcSoft.Core/Collections/Generic/DictionaryExtensions.cs
@@ -1,7 +1,7 @@
 ﻿#region Licence
 
 /****************************************************************************
-          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
 
     Permission is granted to anyone to use this software for any purpose on
     any computer system, and to alter it and redistribute it, subject
@@ -67,6 +67,54 @@ namespace WmcSoft.Collections.Generic
                     return value;
             }
             return defaultValue;
+        }
+
+        /// <summary>
+        /// Combines a dictionary with another one, eventually overriding the existing values.
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+        /// <typeparam name="TDictionary">The type of the dictionary source dictionary, for chaining.</typeparam>
+        /// <param name="dictionary">The source dictionary.</param>
+        /// <param name="other">The other dictionary.</param>
+        /// <param name="combiner">A function to combine the values when the key exists in both dictionary.</param>
+        /// <returns>The <paramref name="dictionary"/>.</returns>
+        public static TDictionary CombineWith<TKey, TValue, TDictionary>(this TDictionary dictionary, IDictionary<TKey, TValue> other, Func<TValue, TValue, TValue> combiner)
+            where TDictionary : IDictionary<TKey, TValue> {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+            if (other != null) {
+                foreach (var entry in other) {
+                    TValue existing;
+                    if (dictionary.TryGetValue(entry.Key, out existing)) {
+                        dictionary[entry.Key] = combiner(existing, entry.Value);
+                    } else {
+                        dictionary.Add(entry.Key, entry.Value);
+                    }
+                }
+            }
+            return dictionary;
+        }
+
+        /// <summary>
+        /// Merges a dictionary with another one, eventually overriding the existing values.
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+        /// <typeparam name="TDictionary">The type of the dictionary source dictionary, for chaining.</typeparam>
+        /// <param name="dictionary">The source dictionary.</param>
+        /// <param name="other">The other dictionary.</param>
+        /// <returns>The <paramref name="dictionary"/>.</returns>
+        public static TDictionary MergeWith<TKey, TValue, TDictionary>(this TDictionary dictionary, IDictionary<TKey, TValue> other)
+            where TDictionary : IDictionary<TKey, TValue> {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+            if (other != null) {
+                foreach (var entry in other) {
+                    dictionary[entry.Key] = entry.Value;
+                }
+            }
+            return dictionary;
         }
     }
 }
