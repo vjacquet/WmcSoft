2751a97f3c902685b4d9ebe1d860524f3a852775
vjacquet <vjacquet@flowgroup.fr>
2016-08-02 07:08:36 +0200

    Added ExcludesWith extension on IDictionary<TKey, TValue>.

--- WmcSoft.Core/Collections/Generic/DictionaryExtensions.cs
+++ WmcSoft.Core/Collections/Generic/DictionaryExtensions.cs
@@ -70,26 +70,45 @@ namespace WmcSoft.Collections.Generic
         }
 
         /// <summary>
-        /// Combines a dictionary with another one, eventually overriding the existing values.
+        /// Removes from a dictionary values with keys existing in another one.
         /// </summary>
         /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
         /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
         /// <typeparam name="TDictionary">The type of the dictionary source dictionary, for chaining.</typeparam>
         /// <param name="dictionary">The source dictionary.</param>
         /// <param name="other">The other dictionary.</param>
-        /// <param name="combiner">A function to combine the values when the key exists in both dictionary.</param>
         /// <returns>The <paramref name="dictionary"/>.</returns>
-        public static TDictionary CombineWith<TKey, TValue, TDictionary>(this TDictionary dictionary, IDictionary<TKey, TValue> other, Func<TValue, TValue, TValue> combiner)
+        public static TDictionary ExceptWith<TKey, TValue, TDictionary>(this TDictionary dictionary, IDictionary<TKey, TValue> other)
+            where TDictionary : IDictionary<TKey, TValue> {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+            if (other != null) {
+                foreach (var entry in other) {
+                    dictionary.Remove(entry.Key);
+                }
+            }
+            return dictionary;
+        }
+
+        /// <summary>
+        /// Removes from a dictionary values with keys existing in another one when the values compares equal.
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+        /// <typeparam name="TDictionary">The type of the dictionary source dictionary, for chaining.</typeparam>
+        /// <param name="dictionary">The source dictionary.</param>
+        /// <param name="other">The other dictionary.</param>
+        /// <param name="comparer">A function to combine the values when the key exists in both dictionary.</param>
+        /// <returns>The <paramref name="dictionary"/>.</returns>
+        public static TDictionary ExceptWith<TKey, TValue, TDictionary>(this TDictionary dictionary, IDictionary<TKey, TValue> other, IEqualityComparer<TValue> comparer)
             where TDictionary : IDictionary<TKey, TValue> {
             if (dictionary == null)
                 throw new ArgumentNullException("dictionary");
             if (other != null) {
                 foreach (var entry in other) {
                     TValue existing;
-                    if (dictionary.TryGetValue(entry.Key, out existing)) {
-                        dictionary[entry.Key] = combiner(existing, entry.Value);
-                    } else {
-                        dictionary.Add(entry.Key, entry.Value);
+                    if (dictionary.TryGetValue(entry.Key, out existing) && comparer.Equals(existing, entry.Value)) {
+                        dictionary.Remove(entry.Key);
                     }
                 }
             }
@@ -112,6 +131,33 @@ namespace WmcSoft.Collections.Generic
             if (other != null) {
                 foreach (var entry in other) {
                     dictionary[entry.Key] = entry.Value;
+                }
+            }
+            return dictionary;
+        }
+
+        /// <summary>
+        /// Merges a dictionary with another one, eventually merging the existing values.
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+        /// <typeparam name="TDictionary">The type of the dictionary source dictionary, for chaining.</typeparam>
+        /// <param name="dictionary">The source dictionary.</param>
+        /// <param name="other">The other dictionary.</param>
+        /// <param name="merger">A function to combine the values when the key exists in both dictionary.</param>
+        /// <returns>The <paramref name="dictionary"/>.</returns>
+        public static TDictionary MergeWith<TKey, TValue, TDictionary>(this TDictionary dictionary, IDictionary<TKey, TValue> other, Func<TValue, TValue, TValue> merger)
+            where TDictionary : IDictionary<TKey, TValue> {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+            if (other != null) {
+                foreach (var entry in other) {
+                    TValue existing;
+                    if (dictionary.TryGetValue(entry.Key, out existing)) {
+                        dictionary[entry.Key] = merger(existing, entry.Value);
+                    } else {
+                        dictionary.Add(entry.Key, entry.Value);
+                    }
                 }
             }
             return dictionary;
