ca3134c0e53ebeda7423b3455bf20479e79034b0
vjacquet <vjacquet@flowgroup.fr>
2016-08-11 16:55:43 +0200

    Added AsLocalTime, AsUniversalTime and Truncate date/time extensions.

--- WmcSoft.Core/DateTimeExtensions.cs
+++ WmcSoft.Core/DateTimeExtensions.cs
@@ -36,6 +36,42 @@ namespace WmcSoft
     public static class DateTimeExtensions
     {
         /// <summary>
+        /// Converts a time to universal time.
+        /// </summary>
+        /// <param name="dateTime">The time.</param>
+        /// <returns>The time as Utc.</returns>
+        /// <remarks>When the kind is unspecified, assumes <see cref="DateTimeKind.Utc"/>.</remarks>
+        public static DateTime AsLocalTime(this DateTime dateTime) {
+            switch (dateTime.Kind) {
+            case DateTimeKind.Local:
+                return dateTime;
+            case DateTimeKind.Utc:
+                return dateTime.ToLocalTime();
+            case DateTimeKind.Unspecified:
+            default:
+                return DateTime.SpecifyKind(dateTime, DateTimeKind.Local);
+            }
+        }
+
+        /// <summary>
+        /// Converts a time to universal time.
+        /// </summary>
+        /// <param name="dateTime">The time.</param>
+        /// <returns>The time as Utc.</returns>
+        /// <remarks>When the kind is unspecified, assumes <see cref="DateTimeKind.Utc"/>.</remarks>
+        public static DateTime AsUniversalTime(this DateTime dateTime) {
+            switch (dateTime.Kind) {
+            case DateTimeKind.Local:
+                return dateTime.ToUniversalTime();
+            case DateTimeKind.Utc:
+                return dateTime;
+            case DateTimeKind.Unspecified:
+            default:
+                return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+            }
+        }
+
+        /// <summary>
         /// Gets the week of the month of the date represented by this instance.
         /// </summary>
         /// <param name="date">The date</param>
@@ -96,6 +132,18 @@ namespace WmcSoft
         public static DateTime LastDayOfWeek(this DateTime date, IFormatProvider formatProvider = null) {
             return UnguardedFirstDayOfWeek(date, formatProvider ?? CultureInfo.CurrentCulture)
                 .AddDays(6);
+        }
+
+        /// <summary>
+        /// <para>Truncates a <see cref="DateTime"/> to a specified resolution.</para>
+        /// <para>A convenient source for resolution is TimeSpan.TicksPerXXXX constants.</para>
+        /// </summary>
+        /// <param name="date">The DateTime object to truncate.</param>
+        /// <param name="resolution">e.g. to round to nearest second, <see cref="TimeSpan.TicksPerSecond"/>.</param>
+        /// <returns>The truncated date.</returns>
+        /// <remarks>Found at <http://stackoverflow.com/questions/1004698/how-to-truncate-milliseconds-off-of-a-net-datetime>.</remarks>
+        public static DateTime Truncate(this DateTime date, long resolution) {
+            return new DateTime(date.Ticks - (date.Ticks % resolution), date.Kind);
         }
     }
 }
