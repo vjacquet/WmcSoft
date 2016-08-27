92cf5c4a8904c35bf7af456cf0b618ba6ce7a84b
vjacquet <vjacquet@flowgroup.fr>
2016-08-27 08:57:19 +0200

    Added SparseArray collection.

---
+++ WmcSoft.Core.Tests/Collections/Specialized/SparseArrayTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft.Collections.Specialized
+{
+    [TestClass]
+    public class SparseArrayTests
+    {
+        [TestMethod]
+        public void CheckExtent() {
+            var array = new SparseArray<string>(100, "?");
+            array[24] = "24";
+            array[16] = "16";
+            array[33] = "33";
+            array[64] = "64";
+            array[48] = "48";
+            var actual = array.Extent;
+            var expected = Pair.Create(16, 64);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckSpareArray() {
+            var array = new SparseArray<string>(100, "?");
+            array[24] = "24";
+            array[16] = "16";
+            array[33] = "33";
+            array[64] = "64";
+            array[48] = "48";
+
+            Assert.AreEqual(100, array.Count);
+            Assert.AreEqual("33", array[33]);
+            Assert.AreEqual("?", array[27]);
+            Assert.AreEqual("?", array[99]);
+        }
+
+        [TestMethod]
+        public void CheckIndexOf() {
+            var array = new SparseArray<string>(100, "?");
+            array[24] = "24";
+            array[16] = "16";
+            array[33] = "33";
+            array[64] = "64";
+            array[48] = "48";
+
+            Assert.AreEqual(24, array.IndexOf("24"));
+            Assert.AreEqual(16, array.IndexOf("16"));
+            Assert.AreEqual(64, array.IndexOf("64"));
+            Assert.AreEqual(0, array.IndexOf("?"));
+            array[0] = "0";
+            array[1] = "1";
+            array[2] = "2";
+            Assert.AreEqual(3, array.IndexOf("?"));
+            Assert.AreEqual(-1, array.IndexOf("missing"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void CheckIndexOutOfBounds() {
+            var array = new SparseArray<string>(100, "?");
+            array[24] = "24";
+            array[16] = "16";
+            array[33] = "33";
+            array[64] = "64";
+            array[48] = "48";
+
+            Assert.AreEqual("?", array[99]);
+            Assert.AreEqual("?", array[100]);
+
+            Assert.Inconclusive();
+        }
+    }
+}
---
+++ WmcSoft.Core/Collections/Specialized/SparseArray.cs
@@ -0,0 +1,152 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WmcSoft.Collections.Specialized
+{
+    public class SparseArray<T> : IList<T>, IReadOnlyList<T>
+    {
+        private readonly SortedDictionary<int, T> _indexes;
+        private readonly T _defaultValue;
+        private int _count;
+
+        public SparseArray(int length, T defaultValue = default(T)) {
+            if (length < 0) throw new ArgumentException(nameof(length));
+
+            _indexes = new SortedDictionary<int, T>();
+            _defaultValue = defaultValue;
+            _count = length;
+        }
+
+        public T this[int index] {
+            get {
+                if (index < 0 || index >= _count) throw new IndexOutOfRangeException();
+
+                T value;
+                if (_indexes.TryGetValue(index, out value))
+                    return value;
+                return _defaultValue;
+            }
+            set {
+                if (index < 0 || index >= _count) throw new IndexOutOfRangeException();
+                _indexes[index] = value;
+            }
+        }
+
+        public int Count { get { return _count; } }
+
+        public Pair<int> Extent {
+            get {
+                var lower = -1;
+                var upper = -1;
+                switch (_indexes.Count) {
+                case 0:
+                    break;
+                case 1:
+                    lower = upper = _indexes.Keys.First();
+                    break;
+                default:
+                    using (var enumerator = _indexes.Keys.GetEnumerator()) {
+                        enumerator.MoveNext();
+                        lower = enumerator.Current;
+                        int countdown = _indexes.Count;
+                        while (--countdown != 0)
+                            enumerator.MoveNext();
+                        upper = enumerator.Current;
+                    }
+                    break;
+                }
+                return new Pair<int>(lower, upper);
+            }
+        }
+
+        public bool IsReadOnly { get { return false; } }
+
+        public void Add(T item) {
+            throw new NotSupportedException();
+        }
+
+        public void Clear() {
+            throw new NotSupportedException();
+        }
+
+        public bool Contains(T item) {
+            return IndexOf(item) >= 0;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex) {
+            for (int i = 0; i < _count; i++) {
+                array[arrayIndex + i] = _defaultValue;
+            }
+            foreach (var entry in _indexes) {
+                array[arrayIndex + entry.Key] = entry.Value;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            return _indexes.Values.GetEnumerator();
+        }
+
+        public int IndexOf(T item) {
+            var comparer = EqualityComparer<T>.Default;
+            if (comparer.Equals(item, _defaultValue)) {
+                int i = 0;
+                foreach (var entry in _indexes) {
+                    if (entry.Key != i || comparer.Equals(item, entry.Value))
+                        return i;
+                    i++;
+                }
+            } else {
+                foreach (var entry in _indexes) {
+                    if (comparer.Equals(item, entry.Value)) {
+                        return entry.Key;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item) {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item) {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index) {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+}
