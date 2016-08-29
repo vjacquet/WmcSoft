f5d12129c665231d489f119ca12bc5ccd14a6f25
vjacquet <vjacquet@flowgroup.fr>
2016-08-29 09:34:43 +0200

    Added alphabet and IOrderedDictionary interface.

--- WmcSoft.Core/Text/Alphabet.cs
+++ WmcSoft.Core/Text/Alphabet.cs
@@ -27,15 +27,100 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using static WmcSoft.BitArithmetics;
 
 namespace WmcSoft.Text
 {
     /// <summary>
-    /// 
+    /// Represents an alphabet
     /// </summary>
     public class Alphabet
     {
+        #region Known alphabets
+
+        public static readonly Alphabet Binary = new Alphabet("01");
+        public static readonly Alphabet Octal = new Alphabet("01234567");
+        public static readonly Alphabet Decimal = new Alphabet("0123456789");
+        public static readonly Alphabet HexaDecimal = new Alphabet("0123456789ABCDEF");
+        public static readonly Alphabet DNA = new Alphabet("ACGT");
+        public static readonly Alphabet LowerCase = new Alphabet("abcdefghijklmnopqrstuvwxyz");
+        public static readonly Alphabet UpperCase = new Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        public static readonly Alphabet Protein = new Alphabet("ACDEFGHIKLMNPQRSTVWY");
+        public static readonly Alphabet Base64 = new Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
+        public static readonly Alphabet ASCII = new Alphabet(128);
+        public static readonly Alphabet ExtendedASCII = new Alphabet(256);
+        public static readonly Alphabet Unicode = new Alphabet(65536);
+
+        #endregion
+
+        private readonly char[] _alphabet;
+        private readonly int[] _inverse;
+        private readonly int _radix;
+
+        static void ThrowOnDuplicate(IEnumerable<char> alphabet) {
+            var unicode = new bool[Char.MaxValue];
+            foreach (var c in alphabet) {
+                if (unicode[c])
+                    throw new ArgumentException(nameof(alphabet));
+                unicode[c] = true;
+            }
+        }
+
+        public Alphabet(string alphabet) {
+            if (alphabet == null) throw new ArgumentNullException(nameof(alphabet));
+            ThrowOnDuplicate(alphabet);
+
+            _alphabet = alphabet.ToCharArray();
+            _radix = alphabet.Length;
+
+            _inverse = new int[Char.MaxValue];
+            for (int i = 0; i < _inverse.Length; i++)
+                _inverse[i] = -1;
+
+            for (int c = 0; c < _radix; c++)
+                _inverse[alphabet[c]] = c;
+        }
+
+        private Alphabet(int radix) {
+            _radix = radix;
+            _alphabet = new char[radix];
+            _inverse = new int[radix];
+            for (int i = 0; i < _radix; ++i) {
+                _alphabet[i] = (char)i;
+                _inverse[i] = i;
+            }
+        }
+
+        public Alphabet() : this(256) {
+        }
+
+        public bool Contains(char c) {
+            return _inverse[c] != -1;
+        }
+
+        public int Radix { get { return _radix; } }
+
+        public int LbRadix { get { return Lb(Radix); } }
+
+        public int this[char c] {
+            get {
+                var index = _inverse[c];
+                if (index == -1) throw new ArgumentException(nameof(c));
+                return index;
+            }
+        }
+
+        public IEnumerable<int> ToIndices(IEnumerable<char> sequence) {
+            return sequence.Select(c => this[c]).ToArray();
+        }
+
+        public char this[int index] {
+            get { return _alphabet[index]; }
+        }
+
+        public IEnumerable<char> ToChars(IEnumerable<int> sequence) {
+            return sequence.Select(i => _alphabet[i]).ToArray();
+        }
     }
 }
