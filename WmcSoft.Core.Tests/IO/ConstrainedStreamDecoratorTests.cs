2d9ca765d6d849547c2adcbe7919a0e2273b37d6
vjacquet <vjacquet@flowgroup.fr>
2016-09-21 09:15:34 +0200

    Fixed ConstrainedStreamDecorator to inherit traits from base unless overriden and added missing Seek override.

--- WmcSoft.Core.Tests/IO/ConstrainedStreamDecoratorTests.cs
+++ WmcSoft.Core.Tests/IO/ConstrainedStreamDecoratorTests.cs
@@ -7,35 +7,10 @@ namespace WmcSoft.IO
     [TestClass]
     public class ConstrainedStreamDecoratorTests
     {
-        class MockStream : ConstrainedStreamDecorator
-        {
-            private readonly bool _canRead;
-            private readonly bool _canWrite;
-            private readonly bool _canSeek;
-
-            public MockStream(bool canRead = true, bool canWrite = true, bool canSeek = true)
-                : base(new MemoryStream()) {
-                _canRead = canRead;
-                _canWrite = canWrite;
-                _canSeek = canSeek;
-            }
-
-            public override bool CanRead {
-                get { return _canRead; }
-            }
-
-            public override bool CanWrite {
-                get { return _canWrite; }
-            }
-            public override bool CanSeek {
-                get { return _canSeek; }
-            }
-        }
-
         [TestMethod]
         [ExpectedException(typeof(NotSupportedException))]
         public void WriteThrowsWhenCanWriteReturnsFalse() {
-            var stream = new MockStream(canWrite: false);
+            var stream = new MockConstrainedStream(canWrite: false);
             var buffer = new byte[] { 0, 1, 2, 3, 4 };
 
             Assert.AreEqual(false, stream.CanWrite);
@@ -45,11 +20,31 @@ namespace WmcSoft.IO
         [TestMethod]
         [ExpectedException(typeof(NotSupportedException))]
         public void ReadThrowsWhenCanReadReturnsFalse() {
-            var stream = new MockStream(canRead: false);
             var buffer = new byte[] { 0, 1, 2, 3, 4 };
+            var stream = new MockConstrainedStream(new MemoryStream(buffer), canRead: false);
 
             Assert.AreEqual(false, stream.CanRead);
             stream.Read(buffer, 0, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void SeekThrowsWhenCanSeekReturnsFalse() {
+            var buffer = new byte[] { 0, 1, 2, 3, 4 };
+            var stream = new MockConstrainedStream(new MemoryStream(buffer), canRead: true, canSeek: false);
+
+            Assert.AreEqual(false, stream.CanSeek);
+            stream.Seek(0, SeekOrigin.End);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void PositionThrowsWhenCanSeekReturnsFalse() {
+            var buffer = new byte[] { 0, 1, 2, 3, 4 };
+            var stream = new MockConstrainedStream(new MemoryStream(buffer), canRead: true, canSeek: false);
+
+            Assert.AreEqual(false, stream.CanSeek);
+            var position = stream.Position;
         }
     }
 }
