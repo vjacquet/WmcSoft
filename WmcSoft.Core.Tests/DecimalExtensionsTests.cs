da15fcda365be5bcf281a187c348c47a324f800f
vjacquet <vjacquet@flowgroup.fr>
2016-10-12 00:45:21 +0200

    Added Scale decimal extension.

---
+++ WmcSoft.Core.Tests/DecimalExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WmcSoft
+{
+    /// <summary>
+    /// Summary description for DecimalExtensionsTests
+    /// </summary>
+    [TestClass]
+    public class DecimalExtensionsTests
+    {
+        [TestMethod]
+        public void CanGetScale() {
+            Assert.AreEqual(0, 1m.Scale());
+            Assert.AreEqual(4, 1.2345m.Scale());
+            Assert.AreEqual(8, 1.23456789m.Scale());
+            Assert.AreEqual(3, 12.345m.Scale());
+            Assert.AreEqual(1, 12.3456E+3m.Scale());
+        }
+    }
+}
---
+++ WmcSoft.Core/DecimalExtensions.cs
@@ -0,0 +1,51 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmcSoft
+{
+    public static class DecimalExtensions
+    {
+        private const int SignMask = unchecked((int)0x80000000);
+
+        // Scale mask for the flags field. This byte in the flags field contains
+        // the power of 10 to divide the Decimal value by. The scale byte must
+        // contain a value between 0 and 28 inclusive.
+        private const int ScaleMask = 0x00FF0000;
+
+        // Number of bits scale is shifted by.
+        private const int ScaleShift = 16;  
+        public static int Scale(this decimal value) {
+            var bits = decimal.GetBits(value);
+            return (bits[3] & ScaleMask) >> ScaleShift;
+        }
+    }
+}
