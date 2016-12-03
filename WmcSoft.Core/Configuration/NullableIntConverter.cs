35a1886a630930b52275d47c11aea62f6027205d
vjacquet <vjacquet@flowgroup.fr>
2016-12-03 08:36:26 +0100

    Added NullableIntConverter & InfiniteNullableIntConverter.

---
+++ WmcSoft.Core/Configuration/NullableIntConverter.cs
@@ -0,0 +1,48 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2016 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Globalization;
+
+namespace WmcSoft.Configuration
+{
+    public sealed class NullableIntConverter : ConfigurationConverterBase
+    {
+        public override object ConvertFrom(ITypeDescriptorContext ctx, CultureInfo ci, object data) {
+            if (string.IsNullOrEmpty((string)data))
+                return null;
+            return Convert.ToInt32((string)data, 10);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext ctx, CultureInfo ci, object value, Type type) {
+            if (value == null)
+                return "";
+            return ((int)value).ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
