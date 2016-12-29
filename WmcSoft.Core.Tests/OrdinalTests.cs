e7d51cf9ad0f6c4d2077489d3e33aefbc9949358
vjacquet <vjacquet@flowgroup.fr>
2016-12-29 21:10:22 +0100

    Fixed Sequence and Expand Ordinal's extensions.

--- WmcSoft.Core.Tests/OrdinalTests.cs
+++ WmcSoft.Core.Tests/OrdinalTests.cs
@@ -16,6 +16,15 @@ namespace WmcSoft
         }
 
         [TestMethod]
+        public void ChechYearOrdinal() {
+            YearOrdinal ordinal;
+
+            var expected = new DateTime(2017, 02, 28);
+            var actual = ordinal.Advance(new DateTime(2016, 02, 29), 1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void CheckSequenceOrdinal() {
             var ordinal = new SequenceOrdinal<char>('a', 'e', 'i', 'o', 'u', 'y');
             ContractAssert.Ordinal(ordinal, 'a', 'u', 4);
