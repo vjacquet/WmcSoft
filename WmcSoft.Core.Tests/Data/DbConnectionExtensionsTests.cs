302408f02789a7753978293a764fd4e0e2ab8bca
vjacquet <vjacquet@flowgroup.fr>
2017-05-26 08:54:53 +0200

    Tested parametrized commands.

--- WmcSoft.Core.Tests/Data/DbConnectionExtensionsTests.cs
+++ WmcSoft.Core.Tests/Data/DbConnectionExtensionsTests.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
 using System.Data.SqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WmcSoft.Data.Odbc;
+using WmcSoft.Data.SqlClient;
+using WmcSoft.Data.OleDb;
 
 namespace WmcSoft.Data
 {
@@ -9,14 +14,38 @@ namespace WmcSoft.Data
     public class DbConnectionExtensionsTests
     {
         [TestMethod]
-        public void CanCreateParametrizedQuery()
+        public void CanCreateParametrizedSqlQuery()
         {
             var id = 0;
             using (var connection = new SqlConnection())
-            using (var command = connection.CreateParametrizedCommand($"select count(*) from [cms].[pages] where id={id}")) {
+            using (var command = connection.CreateParametrizedCommand($"select count(*) from table where id={id}")) {
                 Assert.AreEqual(1, command.Parameters.Count);
-                Assert.AreEqual("p0", ((IDbDataParameter) command.Parameters[0]).ParameterName);
-                Assert.AreEqual("select count(*) from [cms].[pages] where id=@p0", command.CommandText);
+                Assert.AreEqual("p0", command.Parameters[0].ParameterName);
+                Assert.AreEqual("select count(*) from table where id=@p0", command.CommandText);
+            }
+        }
+
+        [TestMethod]
+        public void CanCreateParametrizedOdbcQuery()
+        {
+            var id = 0;
+            using (var connection = new OdbcConnection())
+            using (var command = connection.CreateParametrizedCommand($"select count(*) from table where id={id}")) {
+                Assert.AreEqual(1, command.Parameters.Count);
+                Assert.AreEqual("p0", command.Parameters[0].ParameterName);
+                Assert.AreEqual("select count(*) from table where id=?", command.CommandText);
+            }
+        }
+
+        [TestMethod]
+        public void CanCreateParametrizedOleDbQuery()
+        {
+            var id = 0;
+            using (var connection = new OleDbConnection())
+            using (var command = connection.CreateParametrizedCommand($"select count(*) from table where id={id}")) {
+                Assert.AreEqual(1, command.Parameters.Count);
+                Assert.AreEqual("p0", command.Parameters[0].ParameterName);
+                Assert.AreEqual("select count(*) from table where id=?", command.CommandText);
             }
         }
     }
