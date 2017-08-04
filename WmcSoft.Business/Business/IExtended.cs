4aa6ee991bf854193d8f88db2ffcc4702bce8111
vjacquet <vjacquet@flowgroup.fr>
2017-08-04 06:59:29 +0200

    Added and tests IExtended interface concept.

---
+++ WmcSoft.Business/Business/IExtended.cs
@@ -0,0 +1,45 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2017 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System.Collections.Generic;
+
+namespace WmcSoft.Business
+{
+    public interface IExtended<I>
+    {
+        /// <summary>
+        /// Gets the extensions.
+        /// </summary>
+        IEnumerable<I> Extensions { get; }
+
+        /// <summary>
+        /// Gets the extension of the specified type. Returns null if no extension of the specified type is configured.
+        /// </summary>
+        /// <typeparam name="TExtension">The type of the extensions</typeparam>
+        /// <returns></returns>
+        TExtension FindExtension<TExtension>() where TExtension : class, I;
+    }
+}
