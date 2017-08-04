f68b2e9a309ed79cf14b59b02c90445c4842f83b
vjacquet <vjacquet@flowgroup.fr>
2017-08-04 06:50:06 +0200

    Added and tests IExtended interface concept.

---
+++ WmcSoft.Core.Tests/Collections/Generic/OrderedListDictionaryTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace WmcSoft.Collections.Generic
+{
+    public class OrderedListDictionaryTests : AssertDictionaryContractTests<string, int>
+    {
+        protected override IDictionary<string, int> CreateDictionary()
+        {
+            return new OrderedListDictionary<string, int>();
+        }
+
+        protected override IEnumerable<KeyValuePair<string, int>> GetSamples()
+        {
+            yield return new KeyValuePair<string, int>("one", 1);
+            yield return new KeyValuePair<string, int>("two", 2);
+            yield return new KeyValuePair<string, int>("three", 3);
+        }
+    }
+}
