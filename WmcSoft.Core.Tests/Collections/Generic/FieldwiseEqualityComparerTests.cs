08cec21bf230097090259c2ffac083a9f46d31f4
vjacquet <vjacquet@flowgroup.fr>
2017-10-23 23:18:50 +0200

    Added FieldwiseEqualityComparer.

---
+++ WmcSoft.Core.Tests/Collections/Generic/FieldwiseEqualityComparerTests.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace WmcSoft.Collections.Generic
+{
+    public class FieldwiseEqualityComparerTests
+    {
+        static readonly IEqualityComparer<Address> Comparer = FieldwiseEqualityComparer<Address>.FromFields();
+
+        private class Address
+        {
+            private readonly string _address1;
+            private readonly string _city;
+            private readonly string _state;
+
+            public Address(string address1, string city, string state)
+            {
+                _address1 = address1;
+                _city = city;
+                _state = state;
+            }
+
+            public string Address1 {
+                get { return _address1; }
+            }
+
+            public string City {
+                get { return _city; }
+            }
+
+            public string State {
+                get { return _state; }
+            }
+        }
+
+        private class ExpandedAddress : Address
+        {
+            private readonly string _address2;
+
+            public ExpandedAddress(string address1, string address2, string city, string state)
+            : base(address1, city, state)
+            {
+                _address2 = address2;
+            }
+
+            public string Address2 {
+                get { return _address2; }
+            }
+        }
+
+        [Fact]
+        public void AddressEqualsWorksWithIdenticalAddresses()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address1", "Austin", "TX");
+
+            Assert.Equal(address, address2, Comparer);
+        }
+
+        [Fact]
+        public void AddressEqualsWorksWithNonIdenticalAddresses()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address2", "Austin", "TX");
+
+            Assert.NotEqual(address, address2, Comparer);
+        }
+
+        [Fact]
+        public void AddressEqualsWorksWithNonIdenticalDerivedAddresses()
+        {
+            var comparer = FieldwiseEqualityComparer<ExpandedAddress>.FromFields();
+            var address = new ExpandedAddress("Address1", "Address2", "Austin", "TX");
+            var address2 = new ExpandedAddress("Address2", "Address2", "Austin", "TX");
+
+            Assert.NotEqual(address, address2, comparer);
+        }
+
+        [Fact]
+        public void AddressEqualsWorksWithNulls()
+        {
+            Address address = new Address(null, "Austin", "TX");
+            Address address2 = new Address("Address2", "Austin", "TX");
+
+            Assert.NotEqual(address, address2, Comparer);
+        }
+
+        [Fact]
+        public void AddressEqualsWorksWithNullsOnOtherObject()
+        {
+            Address address = new Address("Address2", "Austin", "TX");
+            Address address2 = new Address("Address2", null, "TX");
+
+            Assert.NotEqual(address, address2, Comparer);
+        }
+
+        [Fact]
+        public void AddressEqualsIsReflexive()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+
+            Assert.Equal(address, address, Comparer);
+        }
+
+        [Fact]
+        public void AddressEqualsIsSymmetric()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address2", "Austin", "TX");
+
+            Assert.NotEqual(address, address2, Comparer);
+            Assert.NotEqual(address2, address, Comparer);
+        }
+
+        [Fact]
+        public void AddressEqualsIsTransitive()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address1", "Austin", "TX");
+            Address address3 = new Address("Address1", "Austin", "TX");
+
+            Assert.Equal(address, address2, Comparer);
+            Assert.Equal(address2, address3, Comparer);
+            Assert.Equal(address3, address, Comparer);
+        }
+
+        [Fact]
+        public void DerivedTypesBehaveCorrectly()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            ExpandedAddress address2 = new ExpandedAddress("Address1", "Apt 123", "Austin", "TX");
+
+            Assert.NotEqual(address, address2, Comparer);
+        }
+
+        [Fact]
+        public void EqualValueObjectsHaveSameHashCode()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address1", "Austin", "TX");
+
+            Assert.Equal(Comparer.GetHashCode(address), Comparer.GetHashCode(address2));
+        }
+
+        [Fact]
+        public void TransposedValuesGiveDifferentHashCodes()
+        {
+            Address address = new Address(null, "Austin", "TX");
+            Address address2 = new Address("TX", "Austin", null);
+
+            Assert.NotEqual(Comparer.GetHashCode(address), Comparer.GetHashCode(address2));
+        }
+
+        [Fact]
+        public void UnequalValueObjectsHaveDifferentHashCodes()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address2", "Austin", "TX");
+
+            Assert.NotEqual(Comparer.GetHashCode(address), Comparer.GetHashCode(address2));
+        }
+
+        [Fact]
+        public void TransposedValuesOfFieldNamesGivesDifferentHashCodes()
+        {
+            Address address = new Address("_city", null, null);
+            Address address2 = new Address(null, "_address1", null);
+
+            Assert.NotEqual(Comparer.GetHashCode(address), Comparer.GetHashCode(address2));
+        }
+
+        [Fact]
+        public void DerivedTypesHashCodesBehaveCorrectly()
+        {
+            ExpandedAddress address = new ExpandedAddress("Address99999", "Apt 123", "New Orleans", "LA");
+            ExpandedAddress address2 = new ExpandedAddress("Address1", "Apt 123", "Austin", "TX");
+
+            Assert.NotEqual(Comparer.GetHashCode(address), Comparer.GetHashCode(address2));
+        }
+    }
+}
---
+++ WmcSoft.Core/Collections/Generic/FieldwiseEqualityComparer.cs
@@ -0,0 +1,98 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ----------------------------------------------------------------------------
+
+    Adapted from <http://grabbagoft.blogspot.fr/2007/06/generic-value-object-equality.html>
+
+ ****************************************************************************/
+
+#endregion
+
+using System.Collections.Generic;
+using System.Reflection;
+
+using static WmcSoft.Collections.Generic.EqualityComparer;
+
+namespace WmcSoft.Collections.Generic
+{
+    public sealed class FieldwiseEqualityComparer<T> : IEqualityComparer<T>
+    {
+
+        private readonly IList<FieldInfo> _fields;
+
+        public FieldwiseEqualityComparer(IList<FieldInfo> fields)
+        {
+            _fields = fields;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (x == null || y == null) return false;
+            if (x.GetType() != y.GetType()) return false;
+
+            foreach (var field in _fields) {
+                if (!object.Equals(field.GetValue(x), field.GetValue(y)))
+                    return false;
+            }
+            return true;
+        }
+
+        public int GetHashCode(T obj)
+        {
+            if (obj == null) return 0;
+
+            var h = 0;
+            foreach (var field in _fields) {
+                object value = field.GetValue(obj);
+                h = CombineHashCodes(h, value != null ? value.GetHashCode() : 0);
+            }
+            return h;
+        }
+
+        #region Factory methods
+
+        public static FieldwiseEqualityComparer<T> FromFields()
+        {
+            return FromFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        }
+
+        public static FieldwiseEqualityComparer<T> FromFields(BindingFlags bindingFlags)
+        {
+            var fields = typeof(T).GetFields(bindingFlags);
+            if (typeof(T).BaseType == typeof(object))
+                return new FieldwiseEqualityComparer<T>(fields);
+
+            var results = new List<FieldInfo>(fields);
+            var baseType = typeof(T).BaseType;
+            do {
+                results.AddRange(baseType.GetFields(bindingFlags));
+                baseType = baseType.BaseType;
+            } while (baseType != typeof(object));
+
+            return new FieldwiseEqualityComparer<T>(results);
+        }
+
+        #endregion
+    }
+}
