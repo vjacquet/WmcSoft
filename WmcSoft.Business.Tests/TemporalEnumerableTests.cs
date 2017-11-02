3d62671e842a26edcb4e3fa996bbc39feb91db7a
vjacquet <vjacquet@flowgroup.fr>
2017-11-02 20:56:05 +0100

    Added Temporal ITemporalEnumerable and ITemporalIntervalEnumerable.

---
+++ WmcSoft.Business.Tests/TemporalEnumerableTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace WmcSoft.Business
+{
+    public class TemporalEnumerableTests
+    {
+        class Person : ITemporal
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public DateTime? ValidSince { get; set; }
+            public DateTime? ValidUntil { get; set; }
+        }
+
+        public class PersonAddress : ITemporal
+        {
+            public int PersonId { get; set; }
+            public int AddressId { get; set; }
+
+            public DateTime? ValidSince { get; set; }
+            public DateTime? ValidUntil { get; set; }
+        }
+
+        abstract class Address : ITemporal
+        {
+            public int Id { get; set; }
+            public DateTime? ValidSince { get; set; }
+            public DateTime? ValidUntil { get; set; }
+        }
+
+        class PostalAddress : Address
+        {
+            public string[] Lines { get; set; }
+            public string City { get; set; }
+            public string ZipCode { get; set; }
+            public string Country { get; set; }
+        }
+
+        class EmailAddress : Address
+        {
+            public string Email { get; set; }
+        }
+
+        class TelecomAddress : Address
+        {
+            public string Number { get; set; }
+        }
+
+        private readonly List<Person> Persons = new List<Person> {
+            new Person { Id=1, Name="Vincent JAQUET", ValidSince = new DateTime(2017, 01, 01), ValidUntil = new DateTime(2017, 05, 01)},
+            new Person { Id=2, Name="Sandrine JACQUET", ValidSince = new DateTime(2017, 02, 01)},
+            new Person { Id=1, Name="Vincent JACQUET", ValidSince = new DateTime(2017, 05, 01)},
+            new Person { Id=3, Name="Alicia JACQUET", ValidSince = new DateTime(2017, 06, 01)},
+        };
+
+        [Fact]
+        public void CanEnumerateTemporalAsOf()
+        {
+            var v1 = Persons.AsOf(new DateTime(2017, 01, 15)).Select(p => p.Name);
+            var v2 = Persons.AsOf(new DateTime(2017, 02, 15)).Select(p => p.Name);
+            var v3 = Persons.AsOf(new DateTime(2017, 05, 15)).Select(p => p.Name);
+
+            Assert.Equal(new[] { "Vincent JAQUET" }, v1);
+            Assert.Equal(new[] { "Vincent JAQUET", "Sandrine JACQUET" }, v2);
+            Assert.Equal(new[] { "Sandrine JACQUET", "Vincent JACQUET" }, v3);
+        }
+
+
+        [Fact]
+        public void CanEnumerateTemporalBetween()
+        {
+            var actual = Persons.Between(new DateTime(2017, 01, 15), new DateTime(2017, 05, 15)).Select(p => p.Name);
+
+            Assert.Equal(new[] { "Vincent JAQUET", "Sandrine JACQUET", "Vincent JACQUET" }, actual);
+        }
+
+    }
+}
---
+++ WmcSoft.Business/Business/Temporal.Enumerable.cs
@@ -0,0 +1,118 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WmcSoft.Business
+{
+    /// <summary>
+    /// Defines the extension methods to the <see cref="ITemporal"/> interface.
+    /// This is a static class.
+    /// </summary>
+    public static partial class Temporal
+    {
+        class AsOfEnumerable<T> : ITemporalEnumerable<T>
+            where T : ITemporal
+        {
+            private readonly IEnumerable<T> _filtered;
+
+            public AsOfEnumerable(DateTime asOf, IEnumerable<T> source)
+            {
+                AsOf = asOf;
+                _filtered = source.Where(x => x.IsValidOn(asOf));
+            }
+
+            public DateTime AsOf { get; }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                return _filtered.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        class BetweenEnumerable<T> : ITemporalIntervalEnumerable<T>
+            where T : ITemporal
+        {
+            private readonly IEnumerable<T> _filtered;
+
+            public BetweenEnumerable(DateTime since, DateTime until, IEnumerable<T> source)
+            {
+                Since = since;
+                Until = until;
+                _filtered = source.Where(x => x.IsValidOn(since, until));
+            }
+
+            public DateTime Since { get; }
+            public DateTime Until { get; }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                return _filtered.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        public static ITemporalEnumerable<T> AsOf<T>(this IEnumerable<T> source, DateTime date)
+             where T : ITemporal
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            return new AsOfEnumerable<T>(date, source);
+        }
+
+        public static ITemporalIntervalEnumerable<T> Between<T>(this IEnumerable<T> source, DateTime since, DateTime until)
+             where T : ITemporal
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            return new BetweenEnumerable<T>(since, until, source);
+        }
+    }
+
+    public interface ITemporalEnumerable<out T> : IEnumerable<T>
+        where T : ITemporal
+    {
+        DateTime AsOf { get; }
+    }
+
+    public interface ITemporalIntervalEnumerable<out T> : IEnumerable<T>
+    {
+        DateTime Since { get; }
+        DateTime Until { get; }
+    }
+}
