4ae874164a57780d045a66938d49bc1ba2182446
Vincent JACQUET <vjacquet@flowgroup.fr>
2017-11-25 08:46:37 +0100

    Fixed build on computer with only VS2017.

--- Tools/WmcSoft.VisualStudio/VisualStudio2013Package.cs
+++ Tools/WmcSoft.VisualStudio/VisualStudio2013Package.cs
@@ -7,7 +7,7 @@ namespace WmcSoft
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [ProvideObject(typeof(CustomTools.CodeGenerator))]
-    [ProvideGeneratorAttribute(typeof(CustomTools.CodeGenerator), "CodeGenerator", "Declarative code generator", "{FAE04EC1-301F-11d3-BF4B-00C04F79EFBC}", true)]
+    [ProvideGenerator(typeof(CustomTools.CodeGenerator), "CodeGenerator", "Declarative code generator", "{FAE04EC1-301F-11d3-BF4B-00C04F79EFBC}", true)]
     [DefaultRegistryRoot("SOFTWARE\\Microsoft\\VisualStudio\\12.0")]
     [Guid(GuidList.guidVSPackagePkgString)]
     public sealed class VisualStudio2013Package : Package
