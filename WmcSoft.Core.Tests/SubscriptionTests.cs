3ecbeafb421638ec67056c5ee1fa3bf6c7aa9bc2
Vincent JACQUET <vjacquet@flowgroup.fr>
2017-12-09 18:06:07 +0100

    Added unit tests for Scubscription.

---
+++ WmcSoft.Core.Tests/SubscriptionTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace WmcSoft
+{
+    public class SubscriptionTests
+    {
+        [Fact]
+        public void CanUnsubscribe()
+        {
+            int count = 0;
+
+            var subscripton = new Subscription(() => count++);
+            Assert.Equal(0, count);
+
+            subscripton.Unsubscribe();
+            Assert.Equal(1, count);
+
+            subscripton.Unsubscribe();
+            Assert.Equal(1, count);
+        }
+    }
+}
