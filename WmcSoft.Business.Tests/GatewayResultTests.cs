d95cd61c5d01d203937220ed402ef6e80080bc99
Vincent JACQUET <vjacquet@flowgroup.fr>
2018-04-28 04:19:19 +0200

    Added support for value in GatewayResult.

--- WmcSoft.Business.Tests/GatewayResultTests.cs
+++ WmcSoft.Business.Tests/GatewayResultTests.cs
@@ -23,6 +23,22 @@ namespace WmcSoft.Business
         }
 
         [Fact]
+        public void CanRoundTripUndefinedGatewayResult()
+        {
+            var undefined = GatewayResult.Failed();
+            var serializer = new BinarySerializer<GatewayResult>();
+            var ms = new MemoryStream();
+
+            serializer.Serialize(ms, undefined);
+            ms.Rewind();
+
+            var result = serializer.Deserialize(ms);
+            Assert.False(undefined.Succeeded);
+            Assert.NotNull(undefined.Errors);
+            Assert.Empty(undefined.Errors);
+        }
+
+        [Fact]
         public void CanRoundTripSuccessGatewayResult()
         {
             var ok = GatewayResult.Success;
@@ -34,6 +50,68 @@ namespace WmcSoft.Business
 
             var result = serializer.Deserialize(ms);
             Assert.True(result.Succeeded);
+        }
+
+        [Fact]
+        public void CanConstructGatewayResultWithValue()
+        {
+           var ok = GatewayResult.Returns(42);
+           var undefined = GatewayResult<int>.Failed();
+           var failed = GatewayResult<int>.Failed(new GatewayError { Code = "Error" });
+
+            Assert.True(ok.Succeeded);
+            Assert.Equal(42, ok.Value);
+            Assert.False(undefined.Succeeded);
+            Assert.NotNull(undefined.Errors);
+            Assert.Empty(undefined.Errors);
+            Assert.False(failed.Succeeded);
+            Assert.Single(failed.Errors);
+        }
+
+        [Fact]
+        public void CanRoundTripSuccessGatewayResultWithValue()
+        {
+            var ok = GatewayResult.Returns(42);
+            var serializer = new BinarySerializer<GatewayResult<int>>();
+            var ms = new MemoryStream();
+
+            serializer.Serialize(ms, ok);
+            ms.Rewind();
+
+            var result = serializer.Deserialize(ms);
+            Assert.True(result.Succeeded);
+        }
+
+        [Fact]
+        public void CanRoundTripUndefinedGatewayResultWithValue()
+        {
+            var undefined = GatewayResult<int>.Failed();
+            var serializer = new BinarySerializer<GatewayResult<int>>();
+            var ms = new MemoryStream();
+
+            serializer.Serialize(ms, undefined);
+            ms.Rewind();
+
+            var result = serializer.Deserialize(ms);
+            Assert.False(undefined.Succeeded);
+            Assert.NotNull(undefined.Errors);
+            Assert.Empty(undefined.Errors);
+        }
+
+        [Fact]
+        public void CanRoundTripFailedGatewayResultWithValue()
+        {
+          var failed = GatewayResult<int>.Failed(new GatewayError { Code = "Error" });
+            var serializer = new BinarySerializer<GatewayResult<int>>();
+            var ms = new MemoryStream();
+
+            serializer.Serialize(ms, failed);
+            ms.Rewind();
+
+            var result = serializer.Deserialize(ms);
+            Assert.False(result.Succeeded);
+            Assert.NotEmpty(result.Errors);
+            Assert.Equal("Error", result.Errors[0].Code);
         }
     }
 }
--- WmcSoft.Business/GatewayResult.cs
+++ WmcSoft.Business/GatewayResult.cs
@@ -33,16 +33,16 @@ using WmcSoft.Runtime.Serialization;
 namespace WmcSoft
 {
     /// <summary>
-    /// Represents the result of a gateway operation.
+    /// Represents the outcome of a gateway operation that does not return a value.
     /// </summary>
     [DebuggerDisplay("{ToString(),nq}")]
     [Serializable]
-    public sealed class GatewayResult : ISerializable
+    public struct GatewayResult : ISerializable
     {
-        private static GatewayError[] None = new GatewayError[0];
-        private static GatewayError[] Undefined = new GatewayError[0];
+        internal static GatewayError[] None = new GatewayError[0];
+        internal static GatewayError[] Undefined = new GatewayError[0];
 
-        private readonly GatewayError[] _errors;
+        internal readonly GatewayError[] _errors;
 
         private GatewayResult(GatewayError[] errors)
         {
@@ -62,6 +62,34 @@ namespace WmcSoft
         }
 
         /// <summary>
+        /// Returns an <see cref="GatewayResult{T}"/> indicating a successful operation.
+        /// </summary>
+        /// <typeparam name="T">The type of the value in the operation result.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns>The operation result.</returns>
+        public static GatewayResult<T> Returns<T>(T value)
+        {
+            return new GatewayResult<T>(value);
+        }
+
+        /// <summary>
+        /// Throws an <see cref="InvalidOperationException"/> if the result contains errors.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The operation result contains errors.</exception>
+        public void ThrowIfFailed()
+        {
+            if (Succeeded)
+                return;
+            if (ReferenceEquals(_errors, null))
+                throw new InvalidOperationException();
+
+            var message = string.Join("\r\n", Array.ConvertAll(_errors, e => e.Description ?? e.Code));
+            var exception = new InvalidOperationException(message);
+            exception.Data.Add("Errors", _errors);
+            throw exception;
+        }
+
+        /// <summary>
         /// Errors that occured during the operation.
         /// </summary>
         public ReadOnlyCollection<GatewayError> Errors {
@@ -69,7 +97,7 @@ namespace WmcSoft
         }
 
         /// <summary>
-        /// Creates a failed  <see cref="GatewayResult"/> with a list of errors.
+        /// Creates a failed <see cref="GatewayResult"/> with a list of errors.
         /// </summary>
         /// <param name="errors">The list of errors.</param>
         /// <returns>The result.</returns>
@@ -104,6 +132,163 @@ namespace WmcSoft
             } else {
                 info.AddValue("Succeeded", false);
                 info.AddValue("Errors", _errors);
+            }
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Represents the outcome of a gateway operation that returns a value.
+    /// </summary>
+    [DebuggerDisplay("{ToString(),nq}")]
+    [Serializable]
+    public struct GatewayResult<T> : ISerializable
+    {
+        private GatewayResult _result;
+        private readonly T _value;
+
+        public GatewayResult(T value)
+        {
+            _result = GatewayResult.Success;
+            _value = value;
+        }
+
+        private GatewayResult(GatewayError[] errors)
+        {
+            _result = GatewayResult.Failed(errors);
+            _value = default(T);
+        }
+
+        /// <summary>
+        /// Retrieves the value of the current <see cref="GatewayResult{T}"/> object, or the object's default value.
+        /// </summary>
+        /// <returns>The value of the Value property if the <see cref="HasValue"/> property is <c>true</c>; otherwise,
+        /// the default value of the current <see cref="GatewayResult{T}"/> object.
+        /// The type of the default value is the type argument of the current <see cref="GatewayResult{T}"/> object,
+        /// and the value of the default value consists solely of binary zeroes.</returns>
+        /// <remarks>The <see cref="GetValueOrDefault"/> method returns a value even if the <see cref="HasValue"/> property is <c>false</c>
+        /// (unlike the <see cref="Value"/> property, which throws an exception).</remarks>
+        public T GetValueOrDefault()
+        {
+            return _value;
+        }
+
+        /// <summary>
+        /// Retrieves the value of the current <see cref="GatewayResult{T}"/> object, or the specified default value.
+        /// </summary>
+        /// <returns>The value of the Value property if the <see cref="HasValue"/> property is <c>true</c>; otherwise,
+        /// the <paramref name="defaultValue"/> parameter.
+        /// The type of the default value is the type argument of the current <see cref="GatewayResult{T}"/> object,
+        /// and the value of the default value consists solely of binary zeroes.</returns>
+        /// <remarks>The <see cref="GetValueOrDefault"/> method returns a value even if the <see cref="HasValue"/> property is <c>false</c>
+        /// (unlike the <see cref="Value"/> property, which throws an exception).</remarks>
+        public T GetValueOrDefault(T defaultValue)
+        {
+            return HasValue ? _value : defaultValue;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the current <see cref="GatewayResult{T}"/> object has a valid value of its underlying type.
+        /// </summary>
+        /// <value><c>true</c> if the current <see cref="GatewayResult{T}"/> object has a value; <c>false</c> if the current<see cref="GatewayResult{T}"/> object has no value.</value>
+        public bool HasValue => Succeeded;
+
+        /// <summary>
+        /// Gets the value of the current <see cref="GatewayResult{T}"/> object if it has been assigned a valid underlying value.
+        /// </summary>
+        /// <value></value>
+        /// <exception cref="InvalidOperationException">The <see cref="HasValue"/> property is <c>false</c>.</exception>
+        public T Value {
+            get {
+                _result.ThrowIfFailed();
+                return _value;
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether the operation succeeded or not.
+        /// </summary>
+        public bool Succeeded {
+            get { return _result.Succeeded; }
+        }
+
+        public static explicit operator T(GatewayResult<T> result)
+        {
+            return result.Value;
+        }
+
+        public static implicit operator GatewayResult<T>(T value)
+        {
+            return new GatewayResult<T>(value);
+        }
+
+        /// <summary>
+        /// Converts a <see cref="GatewayResult"/> to a <see cref="GatewayResult{T}"/>. If the <paramref name="result"/> result was successfull,
+        /// the returned value contains the type's default value.
+        /// </summary>
+        /// <param name="result">The <see cref="GatewayResult"/> to convert.</param>
+        /// <remarks>This conversion is useful to convert failed results.</remarks>
+        public static implicit operator GatewayResult<T>(GatewayResult result)
+        {
+            return new GatewayResult<T>(result._errors);
+        }
+
+        /// <summary>
+        /// Throws an <see cref="InvalidOperationException"/> if the result contains errors.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The operation result contains errors.</exception>
+
+        public void ThrowIfFailed()
+        {
+            _result.ThrowIfFailed();
+        }
+
+        /// <summary>
+        /// Errors that occured during the operation.
+        /// </summary>
+        public ReadOnlyCollection<GatewayError> Errors => _result.Errors;
+
+        /// <summary>
+        /// Creates a failed <see cref="GatewayResult"/> with a list of errors.
+        /// </summary>
+        /// <param name="errors">The list of errors.</param>
+        /// <returns>The result.</returns>
+        public static GatewayResult<T> Failed(params GatewayError[] errors)
+        {
+            if (errors == null || errors.Length == 0)
+                return new GatewayResult<T>(GatewayResult.Undefined);
+            return new GatewayResult<T>((GatewayError[])errors.Clone());
+        }
+
+        public override string ToString()
+        {
+            if (Succeeded)
+                return _value.ToString();
+            return _result.ToString();
+        }
+
+        #region Serialization
+
+        private GatewayResult(SerializationInfo info, StreamingContext context)
+        {
+            var succeeded = info.GetBoolean("Succeeded");
+            if (succeeded) {
+                _value = info.GetValue<T>("Value");
+                _result = GatewayResult.Success;
+            } else {
+                _result = GatewayResult.Failed(info.GetValue<GatewayError[]>("Errors"));
+                _value = default;
+            }
+        }
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (Succeeded) {
+                info.AddValue("Succeeded", true);
+                info.AddValue("Value", _value);
+            } else {
+                ((ISerializable)_result).GetObjectData(info, context);
             }
         }
 
