6e6f3d205843ff35fa59ec789915db1762436b8f
Vincent JACQUET <vjacquet@flowgroup.fr>
2018-05-08 07:16:28 +0200

    Added documentation comments.

--- WmcSoft.Core/Collections/Generic/Algorithms/FinderExtensions.cs
+++ WmcSoft.Core/Collections/Generic/Algorithms/FinderExtensions.cs
@@ -28,19 +28,37 @@ using System.Collections.Generic;
 
 namespace WmcSoft.Collections.Generic.Algorithms
 {
+    /// <summary>
+    /// Defines the extension methods in the <see cref="IFinder{T}"/> interface.
+    /// This is a static class.
+    /// </summary>
     public static class FinderExtensions
     {
-        public static int FindFirstOccurence<T>(this IFinder<T> finder, IReadOnlyList<T> t)
+        /// <summary>
+        /// Finds the first occurence of the pattern in the <paramref name="list"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the source list.</typeparam>
+        /// <param name="finder">The finder.</param>
+        /// <param name="list">The list.</param>
+        /// <returns>The index of the next occurence; - or - <c>-1</c> if not found.</returns>
+        public static int FindFirstOccurence<T>(this IFinder<T> finder, IReadOnlyList<T> list)
         {
-            return finder.FindNextOccurence(t, 0);
+            return finder.FindNextOccurence(list, 0);
         }
 
-        public static IEnumerable<int> FindAllOccurences<T>(this IFinder<T> finder, IReadOnlyList<T> t)
+        /// <summary>
+        /// Enumerates all occurrences of the pattern in the <paramref name="list"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the source list.</typeparam>
+        /// <param name="finder">The finder.</param>
+        /// <param name="list">The list.</param>
+        /// <returns>The index of all occurences of the pattern in the list.</returns>
+        public static IEnumerable<int> FindAllOccurences<T>(this IFinder<T> finder, IReadOnlyList<T> list)
         {
-            var found = finder.FindNextOccurence(t, 0);
+            var found = finder.FindNextOccurence(list, 0);
             while (found >= 0) {
                 yield return found;
-                found = finder.FindNextOccurence(t, found + 1);
+                found = finder.FindNextOccurence(list, found + 1);
             }
         }
     }
