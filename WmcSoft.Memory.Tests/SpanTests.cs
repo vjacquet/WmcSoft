437019e46ce0b2e907777228614f46d23d1172b0
Vincent JACQUET <vjacquet@flowgroup.fr>
2018-06-04 08:19:24 +0200

    Added algorithms based on Span<T> and ReadOnlySpan<T>.

---
+++ WmcSoft.Memory.Tests/SpanTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace WmcSoft.Memory
+{
+    public class SpanTests
+    {
+        [Fact]
+        public void CanCreateSpanFromStrings()
+        {
+            var span = "This is a string".AsSpan();
+            Assert.False(span.IsEmpty);
+        }
+    }
+}
