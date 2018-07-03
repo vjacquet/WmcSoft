5d6fd77dcee5c2a72f118e0f5fcf6aa3279e53a8
Vincent JACQUET <vjacquet@flowgroup.fr>
2018-07-03 07:32:33 +0200

    Added a MockObserver for testing.

--- WmcSoft.Monitoring.Tests/Sentries/BasicObserver.cs
+++ WmcSoft.Monitoring.Tests/Sentries/BasicObserver.cs
@@ -16,5 +16,4 @@ namespace WmcSoft.Monitoring.Sentries
         {
         }
     }
-
 }
---
+++ WmcSoft.Monitoring.Tests/Sentries/MockObserver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WmcSoft.Monitoring.Sentries
+{
+    class MockObserver : IObserver<SentryStatus>
+    {
+        public void OnCompleted()
+        {
+            IsCompleted = true;
+        }
+
+        public void OnError(Exception error)
+        {
+            Error = error;
+        }
+
+        public void OnNext(SentryStatus value)
+        {
+            LatestValue = value;
+            ObservedValues++;
+        }
+
+        public bool IsCompleted { get; set; }
+        public Exception Error { get; set; }
+        public SentryStatus LatestValue { get; set; }
+        public int ObservedValues { get; set; }
+    }
+
+}
