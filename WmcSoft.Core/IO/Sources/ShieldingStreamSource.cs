5512435e0cbbe85817c8741f5db26e6d01889895
Vincent JACQUET <vjacquet@flowgroup.fr>
2018-07-17 09:22:08 +0200

    Added ShieldingStreamSource.

---
+++ WmcSoft.Core/IO/Sources/ShieldingStreamSource.cs
@@ -0,0 +1,95 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace WmcSoft.IO.Sources
+{
+    /// <summary>
+    /// Decorates a stream source that catches all exceptions.
+    /// </summary>
+    public class ShieldingStreamSource<TSource> : IStreamSource
+        where TSource : IStreamSource
+    {
+        private readonly TSource underlying;
+
+        public ShieldingStreamSource(TSource source)
+        {
+            underlying = source;
+        }
+
+        public Exception Error { get; private set; }
+
+        public ShieldingStreamSource(Func<TSource> factory)
+        {
+            try {
+                underlying = factory();
+            } catch (Exception e) {
+                Error = e;
+                Trace.TraceError("Failed to initialized the underlying source. {0}: {1}.", e.GetType().FullName, e.Message);
+            }
+        }
+
+        protected TResult Run<TResult>(string methodName, Func<TSource, TResult> func, TResult defaultResult = default)
+        {
+            Error = null; // reset the error
+
+            if (underlying == null)
+                return defaultResult;
+
+            try {
+                return func(underlying);
+            } catch (Exception e) {
+                Error = e;
+                Trace.TraceError("Failed to run `{2}` underlying source. {0}: {1}.", e.GetType().FullName, e.Message, methodName);
+                return defaultResult;
+            }
+        }
+
+        public Stream GetStream()
+        {
+            return Run(nameof(GetStream), s => s.GetStream());
+        }
+
+        public override string ToString()
+        {
+            return underlying != null ? ('[' + underlying.ToString() + ']') : "[null]";
+        }
+    }
+
+    public class ShieldingStreamSource : ShieldingStreamSource<IStreamSource>
+    {
+        public ShieldingStreamSource(IStreamSource source) : base(source)
+        {
+        }
+
+        public ShieldingStreamSource(Func<IStreamSource> factory) : base(factory)
+        {
+        }
+    }
+}
---
+++ WmcSoft.Core/IO/Sources/TimestampShieldingStreamSource.cs
@@ -0,0 +1,64 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2015 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+
+namespace WmcSoft.IO.Sources
+{
+    /// <summary>
+    /// Decorates a stream source, with a timestamp, that catches all exceptions.
+    /// </summary>
+    public class TimestampShieldingStreamSource<TSource> : ShieldingStreamSource<TSource>, ITimestampStreamSource
+        where TSource : ITimestampStreamSource
+    {
+        public TimestampShieldingStreamSource(TSource source)
+            : base(source)
+        {
+        }
+
+        public TimestampShieldingStreamSource(Func<TSource> factory)
+            : base(factory)
+        {
+        }
+
+        public bool SupportTimestamp => Run(nameof(SupportTimestamp), s => s.SupportTimestamp);
+
+        public DateTime? Timestamp => Run(nameof(Timestamp), s => s.Timestamp);
+    }
+
+    public class TimestampShieldingStreamSource : TimestampShieldingStreamSource<ITimestampStreamSource>
+    {
+        public TimestampShieldingStreamSource(ITimestampStreamSource source)
+            : base(source)
+        {
+        }
+
+        public TimestampShieldingStreamSource(Func<ITimestampStreamSource> factory)
+            : base(factory)
+        {
+        }
+    }
+}
