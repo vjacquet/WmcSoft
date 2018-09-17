9433492d6e3fbb68682ab125ec908c5e7b926ae1
Vincent JACQUET <vjacquet@flowgroup.fr>
2018-09-17 14:22:21 +0200

    Added DateTimeSources.

---
+++ WmcSoft.Core/IO/Sources/DateTimeSources.cs
@@ -0,0 +1,50 @@
+﻿#region Licence
+
+/****************************************************************************
+          Copyright 1999-2018 Vincent J. Jacquet.  All rights reserved.
+
+    Permission is granted to anyone to use this software for any purpose on
+    any computer system, and to alter it and redistribute it, subject
+    to the following restrictions:
+
+    1. The author is not responsible for the consequences of use of this
+       software, no matter how awful, even if they arise from flaws in it.
+
+    2. The origin of this software must not be misrepresented, either by
+       explicit claim or by omission.  Since few users ever read sources,
+       credits must appear in the documentation.
+
+    3. Altered versions must be plainly marked as such, and must not be
+       misrepresented as being the original software.  Since few users
+       ever read sources, credits must appear in the documentation.
+
+    4. This notice may not be removed or altered.
+
+ ****************************************************************************/
+
+#endregion
+
+using System;
+
+namespace WmcSoft.IO.Sources
+{
+    public static class DateTimeSources
+    {
+        #region Implementations
+
+        public struct LocalTimeSource : IDateTimeSource
+        {
+            public DateTime Now() => DateTime.Now;
+        }
+
+        public struct UniversalTimeSource : IDateTimeSource
+        {
+            public DateTime Now() => DateTime.UtcNow;
+        }
+
+        #endregion
+
+        public static LocalTimeSource Local { get; }
+        public static UniversalTimeSource Universal { get; }
+    }
+}
