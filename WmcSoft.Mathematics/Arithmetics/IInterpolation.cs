e179bc53f0dd586cba18db98f23fe2531c594061
Vincent JACQUET <vjacquet@flowgroup.fr>
2018-10-19 19:30:22 +0200

    For consistency, AsFunction returns a IFunction adapter and AsFunc returns a Func.

--- WmcSoft.Mathematics/Arithmetics/IInterpolation.cs
+++ WmcSoft.Mathematics/Arithmetics/IInterpolation.cs
@@ -2,6 +2,11 @@
 
 namespace WmcSoft.Arithmetics
 {
+    /// <summary>
+    /// Represents an algorithm to interpolate a value.
+    /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <typeparam name="U">The type of the result.</typeparam>
     public interface IInterpolation<T, U>
     {
         U Interpolate(T value);
@@ -9,9 +14,39 @@ namespace WmcSoft.Arithmetics
 
     public static class InterpolationExtensions
     {
-        public static Func<T, U> AsFunction<T, U>(this IInterpolation<T, U> interpolation)
+        public static Func<T, U> AsFunc<T, U>(this IInterpolation<T, U> interpolation)
         {
-            return (t) => interpolation.Interpolate(t);
+            return interpolation.Interpolate;
         }
+
+        public static InterpolationAdapter<T> AsFunction<T>(this IInterpolation<T, T> interpolation)
+        {
+            return new InterpolationAdapter<T>(interpolation);
+        }
+    }
+
+    public struct InterpolationAdapter<T> : IFunction<T>
+    {
+        private readonly IInterpolation<T, T> underlying;
+
+        public InterpolationAdapter(IInterpolation<T, T> interpolation)
+        {
+            underlying = interpolation;
+        }
+
+        public T Eval(T x)
+        {
+            return underlying.Interpolate(x);
+        }
+
+        public bool Equals(IFunction<T> other)
+        {
+            if (other is InterpolationAdapter<T> that) {
+                return underlying.Equals(that.underlying);
+            }
+            return false;
+        }
+
+
     }
 }
