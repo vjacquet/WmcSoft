bea6c5f41396c381cb4c43556f9a3a301bbb9ad7
Vincent JACQUET <vjacquet@flowgroup.fr>
2018-10-23 07:25:59 +0200

    Added tests.

--- WmcSoft.Core/IO/NativeMethods.cs
+++ WmcSoft.Core/IO/NativeMethods.cs
@@ -8,7 +8,37 @@ namespace WmcSoft.IO
 {
     internal static class NativeMethods
     {
-        [DllImport("kernel32.dll")]
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct LargeInteger
+        {
+            public readonly int Low;
+            public readonly int High;
+
+            public long ToInt64()
+            {
+                return (High * 0x100000000) + Low;
+            }
+
+            public static implicit operator long(LargeInteger x)
+            {
+                return x.ToInt64();
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32StreamID
+        {
+            public uint dwStreamId;
+            public uint dwStreamAttributes;
+            public LargeInteger Size;
+            public uint dwStreamNameSize;
+        }
+
+        [DllImport("kernel32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool BackupRead(SafeFileHandle hFile, ref Win32StreamID pBuffer, uint numberOfBytesToRead, out uint numberOfBytesRead, [MarshalAs(UnmanagedType.Bool)] bool abort, [MarshalAs(UnmanagedType.Bool)] bool processSecurity, ref IntPtr context);
+
+        [DllImport("kernel32", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool BackupRead(SafeFileHandle hFile, IntPtr lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, [MarshalAs(UnmanagedType.Bool)] bool bAbort, [MarshalAs(UnmanagedType.Bool)] bool bProcessSecurity, ref IntPtr lpContext);
 
@@ -17,12 +47,28 @@ namespace WmcSoft.IO
         internal static extern bool BackupSeek(SafeFileHandle hFile, uint dwLowBytesToSeek, uint dwHighBytesToSeek, out uint lpdwLowByteSeeked, out uint lpdwHighByteSeeked, ref IntPtr lpContext);
 
         [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        internal static extern bool LogonUser(String lpszUsername, String lpszDomain, String lpszPassword, int dwLogonType, int dwLogonProvider, out Impersonate.SafeTokenHandle phToken);
+        internal static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, out Impersonate.SafeTokenHandle phToken);
 
         [DllImport("kernel32.dll")]
         [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
         [SuppressUnmanagedCodeSecurity]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool CloseHandle(IntPtr handle);
+
+        internal const short FILE_ATTRIBUTE_NORMAL = 0x80;
+        internal const short INVALID_HANDLE_VALUE = -1;
+        internal const uint GENERIC_READ = 0x80000000;
+        internal const uint GENERIC_WRITE = 0x40000000;
+        internal const uint CREATE_NEW = 1;
+        internal const uint CREATE_ALWAYS = 2;
+        internal const uint OPEN_EXISTING = 3;
+        internal const uint OPEN_ALWAYS = 4;
+
+        // Use interop to call the CreateFile function.
+        // For more information about CreateFile,
+        // see the unmanaged MSDN reference library.
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+
     }
 }
