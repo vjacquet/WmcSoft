5964bb9c134c29dfa1b7023c851e7f660eac4d13
Vincent JACQUET <vjacquet@flowgroup.fr>
2021-05-08 16:20:06 +0200

    Check hack to generate partial methods in c#

--- WmcSoft.CodeDom.Tests/CodeDomExtensionsTests.cs
+++ WmcSoft.CodeDom.Tests/CodeDomExtensionsTests.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using Microsoft.CSharp;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace WmcSoft.CodeDom.Tests
 {
     public class CodeDomExtensionsTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public CodeDomExtensionsTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public void CanCreateTryCatchFinallyBlocks()
         {
@@ -24,6 +36,45 @@ namespace WmcSoft.CodeDom.Tests
             var result = try1.Try(new CodeExpressionStatement(cs1))
                 .Catch<ArgumentException>("e");
             Assert.Equal(try1, result);
+
+            var provider = new CSharpCodeProvider();
+            var sb = new StringBuilder();
+            using (var writer = new IndentedTextWriter(new StringWriter(sb), "    "))
+                provider.GenerateCodeFromCompileUnit(compileUnit, writer, new CodeGeneratorOptions { });
+            _output.WriteLine(sb.ToString());
+        }
+
+        [Fact]
+        public void CanCreatePartialMethod()
+        {
+            var compileUnit = new CodeCompileUnit();
+            var samples = new CodeNamespace("Samples");
+            samples.Imports.Add(new CodeNamespaceImport("System"));
+            compileUnit.Namespaces.Add(samples);
+            var c = new CodeTypeDeclaration("C") { IsPartial = true };
+            samples.Types.Add(c);
+
+            var m = new CodeMemberMethod {
+                Attributes = MemberAttributes.Abstract,
+                Name = "M",
+                ReturnType = new CodeTypeReference()
+            };
+
+            var provider = new CSharpCodeProvider();
+
+            var sb = new StringBuilder();
+            using (var writer = new IndentedTextWriter(new StringWriter(sb), "    "))
+                provider.GenerateCodeFromMember(m, writer, new CodeGeneratorOptions { });
+            var text = "        " + string.Join("partial", sb.ToString().Trim().Split(new[] { "abstract" }, 2, StringSplitOptions.None));
+            var snippet = new CodeSnippetTypeMember(text);
+            c.Members.Add(snippet);
+
+            sb.Clear();
+            using (var writer = new IndentedTextWriter(new StringWriter(sb), "    "))
+                provider.GenerateCodeFromCompileUnit(compileUnit, writer, new CodeGeneratorOptions { });
+            var result = sb.ToString();
+            Assert.NotNull(result);
+            _output.WriteLine(result);
         }
     }
 }
